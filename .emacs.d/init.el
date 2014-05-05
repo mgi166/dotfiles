5708605f93c085d901f65914cba1e4e102a5f86a
mgi166 <skskoari@gmail.com>
2014-05-05 20:51:04 +0900

    * Add a new file "21_iswitchb.el"

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -43,16 +43,6 @@
 (setq recentf-max-saved-items 200)
 (global-set-key "\C-xf" 'recentf-open-files)
 
-;; iswitchb
-(iswitchb-mode 1)
-;; C-f, C-b, C-n, C-p で候補を切り替えることができるように。
-(add-hook 'iswitchb-define-mode-map-hook
-      (lambda ()
-        (define-key iswitchb-mode-map "\C-n" 'iswitchb-next-match)
-        (define-key iswitchb-mode-map "\C-p" 'iswitchb-prev-match)
-        (define-key iswitchb-mode-map "\C-f" 'iswitchb-next-match)
-        (define-key iswitchb-mode-map "\C-b" 'iswitchb-prev-match)))
-
 
 ;; mac-key-mode (CocoaEmacsの時は不要)
 (when (< emacs-major-version 24)
