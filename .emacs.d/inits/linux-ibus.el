acbc2b5188059c2586582186f802ef1f688d3b94
mgi166 <skskoari@gmail.com>
2014-05-06 23:51:19 +0900

    * Add new etc files in inits directory

---
+++ .emacs.d/inits/linux-ibus.el
@@ -0,0 +1,6 @@
+;; ibus(ibusのデフォルトの設定であるC-SPCで入力モード切り替えをset-markに変更@ubuntu)
+(when (eq system-type 'gnu/linux)
+  (require 'ibus)
+  (add-hook 'after-init-hook 'ibus-mode-on)
+  (ibus-define-common-key ?\C-\s nil)
+)
---
+++ .emacs.d/inits/linux-keybinds.el
@@ -0,0 +1,4 @@
+(define-key global-map [(super z)] 'undo)
+(define-key global-map [(super c)] 'copy-region-as-kill)
+(define-key global-map [(super v)] 'yank)
+(define-key global-map [(super x)] 'kill-region)
