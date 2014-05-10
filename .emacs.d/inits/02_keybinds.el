574c83c80840d2015957e34dcbd5955519a23e2a
mgi166 <skskoari@gmail.com>
2014-05-10 19:22:33 +0900

    Add new keybind "recenter"

--- .emacs.d/inits/02_keybinds.el
+++ .emacs.d/inits/02_keybinds.el
@@ -2,12 +2,14 @@
 (when (eq system-type 'darwin)
   (setq mac-option-modifier 'meta))
 
-(define-key global-map "\C-h" 'delete-backward-char) ;; 削除
-(define-key global-map "\C-t" 'other-window)         ;; 他のwindowに切り替える
+(define-key global-map "\C-h" 'delete-backward-char)
+(define-key global-map "\C-t" 'other-window)
 (define-key global-map "\M-p" 'backward-paragraph)
 (define-key global-map "\M-n" 'forward-paragraph)
-(define-key global-map "\C-x\C-k" 'kill-buffer)      ;; buffer close
-(define-key global-map "\C-\M-^" 'indent-region)     ;; indent-region
+(define-key global-map "\C-x\C-k" 'kill-buffer)
+(define-key global-map "\C-\M-^" 'indent-region)
+(define-key global-map "\C-\M-^" 'indent-region)
+(define-key global-map "\C-l" 'recenter)
 
 ;; マウスのホイールスクロールスピードを調節
 (defun scroll-down-with-lines ()
