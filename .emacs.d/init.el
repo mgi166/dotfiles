6e634b3c35f0c30db7fe0c6ec122573230d02b84
mgi166 <skskoari@gmail.com>
2014-05-11 09:08:33 +0900

    * Add "package.el"

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,4 +1,4 @@
-;; 引数のdirectoryとその sub directoryをload-pathに追加
+;; 引数の directory とその sub_directory を load-path に追加
 (defun add-to-load-path (&rest paths)
    (let (path)
      (dolist (path paths paths)
@@ -10,6 +10,13 @@
 (add-to-load-path "elisp")
 (add-to-load-path "site-lisp")
 
+;; package.el
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/"))
+(add-to-list 'package-archives  '("marmalade" . "http://marmalade-repo.org/packages/"))
+(package-initialize)
+
+;; init-loader.el
 (require 'init-loader)
 (setq init-loader-show-log-after-init nil) ;; debug したい時は t にする
 (init-loader-load "~/.emacs.d/inits")
