553fe9c5248cb71b126cac7aec0efdf3143bca2d
mgi166 <skskoari@gmail.com>
2014-05-12 22:39:39 +0900

    * Add replace-colorthemes

---
+++ .emacs.d/inits/cocoa-emacs-replace-colorthemes.el
@@ -0,0 +1,6 @@
+(add-to-list 'custom-theme-load-path
+             (file-name-as-directory "~/.emacs.d/elisp/replace-colorthemes"))
+
+;; load your favorite theme
+(load-theme 'charcoal-black t t)
+(enable-theme 'charcoal-black)
