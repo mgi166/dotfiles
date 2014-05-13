4c6b94b1dc64c180c60e983902909bdc554aad19
mgi166 <skskoari@gmail.com>
2014-05-13 23:42:58 +0900

    * Add nw-mode color-theme

--- .emacs.d/inits/21_tramp.el
+++ .emacs.d/inits/21_tramp.el
@@ -1,3 +1,2 @@
 ;; tramp (リモートサーバーのファイルをローカルにあるように編集できる。パスワードを聞かれないで ssh できることが前提)
 ;; 使い方 C-x C-f /ssh:user@example.com:/path/to/file
-(require 'tramp)
---
+++ .emacs.d/inits/nw-replace-colorthemes.el
@@ -0,0 +1,8 @@
+(load-theme 'manoj-dark t)
+
+;; (add-to-list 'custom-theme-load-path
+;;              (file-name-as-directory "~/.emacs.d/elisp/replace-colorthemes"))
+
+;; ;; load your favorite theme
+;; (load-theme 'subdued t t)
+;; (enable-theme 'subdued t t)
