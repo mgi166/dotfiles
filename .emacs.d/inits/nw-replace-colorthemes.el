35c2011eeca4b74fcfda760e21987fefad49554b
mgi166 <skskoari@gmail.com>
2014-05-14 09:26:18 +0900

    * Add linum color to "nw-replace-colorthemes.el"

--- .emacs.d/inits/nw-replace-colorthemes.el
+++ .emacs.d/inits/nw-replace-colorthemes.el
@@ -1,5 +1,9 @@
 (load-theme 'manoj-dark t)
 
+;; linum の背景色
+(custom-set-faces
+ '(linum ((t (:inherit (shadow default) :background "Gray9")))))
+
 ;; (add-to-list 'custom-theme-load-path
 ;;              (file-name-as-directory "~/.emacs.d/elisp/replace-colorthemes"))
 
