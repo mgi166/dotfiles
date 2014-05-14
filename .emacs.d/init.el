4d271a63f2958e2fcd14c7dfb27890e24584989f
mgi166 <skskoari@gmail.com>
2014-05-14 23:56:29 +0900

    * Modify function "add-to-list"

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -15,7 +15,7 @@
 ;; package.el
 (require 'package)
 (add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/"))
-(add-to-list 'package-archives  '("marmalade" . "http://marmalade-repo.org/packages/"))
+(add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
 (package-initialize)
 
 ;; cask.el
