6ced46e4b884e8bbddd7bb61b32bba922b67ced2
mgi166 <skskoari@gmail.com>
2014-05-17 10:25:05 +0900

    * Add settings "popwin.el" & Add keybinds "anything-git-files.el"

--- .emacs.d/inits/24_anything-git-files.el
+++ .emacs.d/inits/24_anything-git-files.el
@@ -1,2 +1,3 @@
 (require 'anything-git-files)
 (define-key global-map (kbd "C-c C-b") 'anything-git-files)
+(define-key global-map (kbd "C-c b") 'anything-git-files)
--- .emacs.d/inits/26_popwin.el
+++ .emacs.d/inits/26_popwin.el
@@ -3,4 +3,5 @@
 
 (setq anything-samewindow nil)
 (push '("anything" :regexp t :height 0.4) popwin:special-display-config)
+(push '("all" :regexp t :height 0.4) popwin:special-display-config)
 (push '(dired-mode :height 0.4) popwin:special-display-config)
