e4d6933466db6704a41839b1c4f6bdb82e2f9553
mgi166 <skskoari@gmail.com>
2014-05-20 23:34:26 +0900

    * Add "yasnippet.el" & "volatile-highlights.el"

---
+++ .emacs.d/inits/28_yasnippet.el
@@ -0,0 +1,10 @@
+(require 'yasnippet)
+(setq yas-snippet-dirs '("~/.eamcs.d/snippets"))
+(yas-global-mode 1)
+
+;; 既存スニペットを挿入する
+(define-key yas-minor-mode-map (kbd "C-x y i") 'yas-insert-snippet)
+;; 新規スニペットを作成するバッファを用意する
+(define-key yas-minor-mode-map (kbd "C-x y n") 'yas-new-snippet)
+;; 既存スニペットを閲覧・編集する
+(define-key yas-minor-mode-map (kbd "C-x y v") 'yas-visit-snippet-file)
