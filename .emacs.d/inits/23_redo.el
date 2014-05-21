219a853e15e32e62e35dc2e52c67388557abe85c
mgi166 <skskoari@gmail.com>
2014-05-21 19:31:45 +0900

    * rm "23_redo.el"

--- .emacs.d/inits/23_redo.el
+++
@@ -1,7 +0,0 @@
-;; redo+ (emacs 24 だと使えないので注意)
-(require 'redo+)
-(global-set-key (kbd "M-/") 'redo)
-(setq undo-no-redo t) ; 過去のundoがredoされないようにする
-(setq undo-limit 600000)
-(setq undo-strong-limit 900000))
-
