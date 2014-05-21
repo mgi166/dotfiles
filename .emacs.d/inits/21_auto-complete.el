675a824e45407df2d628a738219a2cb924b51c58
mgi166 <skskoari@gmail.com>
2014-05-21 18:45:49 +0900

    * Add "auto-complete.el" & settings

--- .emacs.d/inits/21_auto-complete.el
+++ .emacs.d/inits/21_auto-complete.el
@@ -1,6 +1,21 @@
 ;; auto-complite.el(自動補完機能)
 (require 'auto-complete)
-(global-auto-complete-mode t)
+(require 'auto-complete-config)
+
+(set-default 'ac-sources
+             '(ac-source-abbrev
+               ac-source-dictionary
+               ac-source-yasnippet
+               ac-source-words-in-buffer
+               ac-source-words-in-same-mode-buffers
+               ac-source-semantic))
+
+(ac-config-default)
+
+(dolist (mode '(ruby-mode))
+  (add-to-list 'ac-modes mode))
 
 ;;補完機能
 (setq partial-complication-mode 1)
+
+(global-auto-complete-mode t)
