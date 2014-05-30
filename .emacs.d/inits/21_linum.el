00b73c62010b3e9993541bf05e67d75d59dc4026
mgi166 <skskoari@gmail.com>
2014-05-31 06:57:35 +0900

    * Add linum-delay because of more light

--- .emacs.d/inits/21_linum.el
+++ .emacs.d/inits/21_linum.el
@@ -1,4 +1,8 @@
 ;; linum.el(左側に行数表示)
 (require 'linum)
+(setq linum-delay t)
+(defadvice linum-schedule (around my-linum-schedule () activate)
+  (run-with-idle-timer 0.2 nil #'linum-update-current))
+
 (global-linum-mode t)
 (setq linum-format "%5d ")
