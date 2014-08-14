ac239e5085f117e0f6564a68e55773c0c7589bad
mgi166 <skskoari@gmail.com>
2014-08-14 09:54:14 +0900

    * Add new functions `percol-git-recent-all-branches`, `percol-git-recent-branches`

--- .zsh/function.zsh
+++ .zsh/function.zsh
@@ -22,6 +22,36 @@ if exists percol; then
     bindkey '^R' percol_select_history
 fi
 
+if exists percol; then
+    function percol-git-recent-branches () {
+        local selected_branch=$(git for-each-ref --format='%(refname)' --sort=-committerdate refs/heads | \
+            perl -pne 's{^refs/heads/}{}' | \
+            percol --query "$LBUFFER")
+        if [ -n "$selected_branch" ]; then
+            BUFFER="git checkout ${selected_branch}"
+            zle accept-line
+        fi
+        zle clear-screen
+    }
+    zle -N percol-git-recent-branches
+
+    function percol-git-recent-all-branches () {
+        local selected_branch=$(git for-each-ref --format='%(refname)' --sort=-committerdate refs/heads refs/remotes | \
+            perl -pne 's{^refs/(heads|remotes)/}{}' | \
+            percol --query "$LBUFFER")
+        if [ -n "$selected_branch" ]; then
+            BUFFER="git checkout -t ${selected_branch}"
+            zle accept-line
+        fi
+        zle clear-screen
+    }
+
+    zle -N percol-git-recent-all-branches
+
+    bindkey '^x^b' percol-git-recent-branches
+    bindkey '^xb' percol-git-recent-all-branches
+fi
+
 # z
 . /usr/local/etc/profile.d/z.sh
 function precmd () {
