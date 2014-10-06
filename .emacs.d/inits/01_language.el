b4ec31cade5bdb290f6f9f4fe01fe38ea896c622
mgi166 <skskoari@gmail.com>
2014-10-06 09:59:46 +0900

    * Use ricty

--- .emacs.d/inits/01_language.el
+++ .emacs.d/inits/01_language.el
@@ -8,13 +8,9 @@
 (auto-compression-mode t)
 
 ;; font の設定
-(set-face-attribute 'default nil :family "Inconsolata" :height 150)
-(set-fontset-font (frame-parameter nil 'font)
-                  'japanese-jisx0208
-                  (cons "Ricty Discord" "iso10646-1"))
-(set-fontset-font (frame-parameter nil 'font)
-                  'japanese-jisx0212
-                  (cons "Ricty Discord" "iso10646-1"))
-(set-fontset-font (frame-parameter nil 'font)
-                  'katakana-jisx0201
-                  (cons "Ricty Discord" "iso10646-1"))
+(set-face-attribute 'default nil
+                    :family "Ricty"
+                    :height 160)
+(set-fontset-font
+    nil 'japanese-jisx0208
+        (font-spec :family "Ricty"))
