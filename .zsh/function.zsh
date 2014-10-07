91a8c9ba1d98210ecf316724635cad1eea0a8a34
mgi166 <skskoari@gmail.com>
2014-10-07 10:06:21 +0900

    * Remove `percol` settings

--- .zsh/function.zsh
+++ .zsh/function.zsh
@@ -8,50 +8,6 @@ function dir () {
 
 function exists { which $1 &> /dev/null }
 
-# percol
-# if exists percol; then
-#     function percol_select_history() {
-#         local tac
-#         exists gtac && tac="gtac" || { exists tac && tac="tac" || { tac="tail -r" } }
-#         BUFFER=$(fc -l -n 1 | eval $tac | percol --query "$LBUFFER")
-#         CURSOR=$#BUFFER         # move cursor
-#         zle -R -c               # refresh
-#     }
-
-#     zle -N percol_select_history
-#     bindkey '^R' percol_select_history
-# fi
-
-# if exists percol; then
-#     function percol-git-recent-branches () {
-#         local selected_branch=$(git for-each-ref --format='%(refname)' --sort=-committerdate refs/heads | \
-#             perl -pne 's{^refs/heads/}{}' | \
-#             percol --query "$LBUFFER")
-#         if [ -n "$selected_branch" ]; then
-#             BUFFER="git checkout ${selected_branch}"
-#             zle accept-line
-#         fi
-#         zle clear-screen
-#     }
-#     zle -N percol-git-recent-branches
-
-#     function percol-git-recent-all-branches () {
-#         local selected_branch=$(git for-each-ref --format='%(refname)' --sort=-committerdate refs/heads refs/remotes | \
-#             perl -pne 's{^refs/(heads|remotes)/}{}' | \
-#             percol --query "$LBUFFER")
-#         if [ -n "$selected_branch" ]; then
-#             BUFFER="git checkout -t ${selected_branch}"
-#             zle accept-line
-#         fi
-#         zle clear-screen
-#     }
-
-#     zle -N percol-git-recent-all-branches
-
-#     bindkey '^x^b' percol-git-recent-branches
-#     bindkey '^xb' percol-git-recent-all-branches
-# fi
-
 if exists peco; then
     function peco_select_history() {
         local tac
