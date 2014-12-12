25a848e94a4468fddc5042235aa41e3d08064581
mgi166 <skskoari@gmail.com>
2014-12-12 09:38:56 +0900

    * Add helm-etags+ & helm-gtags

---
+++ .emacs.d/inits/25_helm-etags+.el
@@ -0,0 +1,7 @@
+(require 'helm-etags+)
+
+(global-set-key "\M-." 'helm-etags-select)
+; (global-set-key "\M-." 'helm-etags+-select)
+(global-set-key "\M-*" 'helm-etags+-history)
+(global-set-key "\M-," 'helm-etags+-history-action-go-back)
+(global-set-key "\M-/" 'helm-etags+-history-action-go-forward)
---
+++ .emacs.d/inits/25_helm-gtags.el
@@ -0,0 +1,8 @@
+(require 'helm-gtags)
+;(add-hook 'ruby-mode-hook 'helm-gtags-mode)
+(add-hook 'helm-gtags-mode-hook
+          '(lambda ()
+             (local-set-key (kbd "M-t") 'helm-gtags-find-tag)
+             (local-set-key (kbd "M-r") 'helm-gtags-find-rtag)
+             (local-set-key (kbd "M-s") 'helm-gtags-find-symbol)
+             (local-set-key (kbd "C-t") 'helm-gtags-pop-stack)))
