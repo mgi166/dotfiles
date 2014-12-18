0608119110276aa272b3454ecf42ac5afaeaa3d5
mgi166 <skskoari@gmail.com>
2014-12-18 10:00:05 +0900

    * indent 2 for js2-mode

--- .emacs.d/inits/80_js2-mode.el
+++ .emacs.d/inits/80_js2-mode.el
@@ -2,3 +2,10 @@
 (autoload 'js2-mode "js2-mode" nil t)
 (add-to-list 'auto-mode-alist
              '("\\.js$" . js2-mode))
+
+(add-hook 'js2-mode-hook
+          '(lambda ()
+             (setq js2-basic-offset 2
+                   tab-width        2
+                   indent-tabs-mode nil
+                   js2-cleanup-whitespace nil)))
