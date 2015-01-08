3c3f66109551de2a595ddc0213a5cd5702f87594
mgi166 <skskoari@gmail.com>
2015-01-08 09:38:06 +0900

    * Remove comment and some `PATH`

--- .zsh/env.zsh
+++ .zsh/env.zsh
@@ -13,20 +13,8 @@ export LESSOPEN="| /usr/local/Cellar/source-highlight/3.1.7/bin/src-hilite-lessp
 export EDITOR='/Applications/Emacs.app/Contents/MacOS/bin/emacsclient -nw'
 
 ##### path 関連
-# /usr/local/bin は homebrew で install した formula のsymboliclinkが貼られている。
-# この directory を$PATHに記入することにより、defalut で入っているコマンドより、homebrewで入れたコマンドが常に優先されることに注意。
-# /usr/local/mysql/bin はdmgでmysqlをインストールしたので、pathに追加している。
-
-export PATH=/usr/local/bin:/usr/local/sbin:$PATH:/usr/local/mysql/bin
-
-# .rbenv/libexec/../completions/rbenv.bash:14: command not found: complete と表示される場合は、chshでshellを書き換えて再起動する。
-# source ~/.rbenv/completions/rbenv.zsh
-
+export PATH=/usr/local/bin:$PATH
 export SVN_EDITOR='/Applications/Emacs.app/Contents/MacOS/bin/emacsclient -nw'
-
-# mysql 5.1.69
-alias mysql=/usr/local/mysql/bin/mysql
-alias mysqladmin=/usr/local/mysql/bin/mysqladmin
 
 # tmux
 PS1="$PS1"'$([ -n "$TMUX" ] && tmux setenv TMUXPWD_$(tmux display -p "#I_#P") "$PWD")'
