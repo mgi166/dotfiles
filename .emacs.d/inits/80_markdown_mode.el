890fc023770aa1ffa2034621e66bfeb17af3ee81
mgi166 <skskoari@gmail.com>
2015-02-02 09:58:36 +0900

    * Add markdown_mode hook

---
+++ .emacs.d/inits/80_markdown_mode.el
@@ -0,0 +1,8 @@
+(require 'markdown-mode)
+(defun markdown-custom ()
+  "markdown-mode-hook"
+  (define-key markdown-mode-map (kbd "M-n") 'markdown-forward-paragraph)
+  (define-key markdown-mode-map (kbd "M-p") 'markdown-backward-paragraph))
+
+(add-hook 'markdown-mode-hook
+  '(lambda() (markdown-custom)))
