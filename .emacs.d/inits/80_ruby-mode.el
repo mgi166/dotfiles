a0d3c6f2e606a00cb1163da25c0263701318f504
mgi166 <skskoari@gmail.com>
2015-04-20 15:48:22 +0900

    Add ruby-indent-line

--- .emacs.d/inits/80_ruby-mode.el
+++ .emacs.d/inits/80_ruby-mode.el
@@ -12,10 +12,25 @@
       (setq ruby-insert-encoding-magic-comment t)
     (setq ruby-insert-encoding-magic-comment nil)))
 
+(defadvice ruby-indent-line (after unindent-closing-paren activate)
+  (let ((column (current-column))
+        indent offset)
+    (save-excursion
+      (back-to-indentation)
+      (let ((state (syntax-ppss)))
+        (setq offset (- column (current-column)))
+        (when (and (eq (char-after) ?\))
+                   (not (zerop (car state))))
+          (goto-char (cadr state))
+          (setq indent (current-indentation)))))
+    (when indent
+      (indent-line-to indent)
+      (when (> offset 0) (forward-char offset)))))
+
 ;; デフォルトではマジックコメントを挿入しない
 (toggle-ruby-magic-comment)
 
-(add-hook 'ruby-mode-hook 'ruby-mode-hooks)
+(add-hook 'ruby-mode-hook 'ruby-mode-hooks 'ruby-indent-line)
 (autoload 'ruby-mode "ruby-mode" nil t)
 (add-to-list 'auto-mode-alist
              '("\\.rake$" . ruby-mode))
