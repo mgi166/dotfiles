9d31e77c82b2856d99d35454cd4f507dd6dffc2a
mgi166 <skskoari@gmail.com>
2015-08-11 08:32:27 +0900

    Add hi-line.el

---
+++ .emacs.d/inits/21_hi-line.el
@@ -0,0 +1,8 @@
+(require 'hl-line)
+(defun global-hl-line-timer-function ()
+  (global-hl-line-unhighlight-all)
+  (let ((global-hl-line-mode t))
+    (global-hl-line-highlight)))
+(setq global-hl-line-timer
+      (run-with-idle-timer 0.03 t 'global-hl-line-timer-function))
+;; (cancel-timer global-hl-line-timer)
