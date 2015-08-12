212f4d8853802f37675c2daed6cbd86d5ac4fa09
mgi166 <skskoari@gmail.com>
2015-08-12 11:08:32 +0900

    'helm-grep -> 'wgrep-helm

--- .emacs.d/inits/28_wgrep.el
+++ .emacs.d/inits/28_wgrep.el
@@ -1,8 +1,8 @@
 (require 'wgrep)
-(require 'helm-wgrep)
+(require 'wgrep-helm)
 
 (setq wgrep-auto-save-buffer t)
 (setq wgrep-enable-key "r")
 (autoload 'wgrep-ag-setup "wgrep-ag")
 (add-hook 'ag-mode-hook 'wgrep-ag-setup)
-(define-key ag-mode-map (kbd "r") 'wgrep-change-to-wgrep-mode)
+;(define-key ag-mode-map (kbd "r") 'wgrep-change-to-wgrep-mode)
