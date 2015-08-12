ffe79fc7b1f1c4a3493984e425112dbd076e7721
mgi166 <skskoari@gmail.com>
2015-08-13 00:04:48 +0900

    `C-x #` delete-frame

--- .emacs.d/inits/29_emacsclient.el
+++ .emacs.d/inits/29_emacsclient.el
@@ -4,6 +4,7 @@
 
 ;; C-x C-c で休止
 (global-set-key (kbd "C-x C-c") 'ns-do-hide-emacs)
+(global-set-key (kbd "C-x #") 'delete-frame)
 
 ;; M-x exit で emacs 終了
 (defalias 'exit 'save-buffers-kill-emacs)
