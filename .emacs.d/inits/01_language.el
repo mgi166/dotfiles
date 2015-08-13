59767b8032880fa589dad05d31eb8d62a7795a0a
mgi166 <skskoari@gmail.com>
2015-08-13 22:52:03 +0900

    Fix set-fontset-font arguments * suppress error with `-nw` option

--- .emacs.d/inits/01_language.el
+++ .emacs.d/inits/01_language.el
@@ -11,6 +11,7 @@
 (set-face-attribute 'default nil
                     :family "Ricty"
                     :height 160)
-(set-fontset-font
-    nil 'japanese-jisx0208
-        (font-spec :family "Ricty"))
+
+(set-fontset-font "fontset-default"
+                  'japanese-jisx0208
+                  (font-spec :family "Ricty"))
