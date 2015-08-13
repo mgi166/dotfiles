69a8e32567fee58b450272cc7a013c32c9c6684f
mgi166 <skskoari@gmail.com>
2015-08-13 21:31:52 +0900

    Add helm-ls-git keybinds

--- .emacs.d/inits/25_helm.el
+++ .emacs.d/inits/25_helm.el
@@ -1,6 +1,12 @@
 (require 'helm)
 (require 'helm-config)
+(require 'helm-ls-git)
 
 (define-key helm-map (kbd "C-h") 'delete-backward-char)
 (global-set-key (kbd "M-o") 'helm-occur)
 (global-set-key (kbd "C-;") 'helm-M-x)
+
+(global-set-key (kbd "C-c b") 'helm-ls-git-ls)
+(global-set-key (kbd "C-c C-b") 'helm-ls-git-ls)
+(global-set-key (kbd "C-c d") 'helm-browse-project)
+(global-set-key (kbd "C-c C-d") 'helm-browse-project)
