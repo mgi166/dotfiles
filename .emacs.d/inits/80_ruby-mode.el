61abdb40534384074997ba48c7da06f1b3775d2c
mgi166 <skskoari@gmail.com>
2015-08-13 22:43:08 +0900

    Remove unnecessary function of `toggle-ruby-magic-comment`

--- .emacs.d/inits/80_ruby-mode.el
+++ .emacs.d/inits/80_ruby-mode.el
@@ -5,13 +5,6 @@
 (defun ruby-mode-hooks ()
   (ruby-block-mode t))
 
-(defun toggle-ruby-magic-comment ()
-  "toggle magic comment top of line"
-  (interactive)
-  (if (null ruby-insert-encoding-magic-comment)
-      (setq ruby-insert-encoding-magic-comment t)
-    (setq ruby-insert-encoding-magic-comment nil)))
-
 (setq ruby-deep-indent-paren-style nil)
 
 (defadvice ruby-indent-line (after unindent-closing-paren activate)
@@ -28,9 +21,6 @@
     (when indent
       (indent-line-to indent)
       (when (> offset 0) (forward-char offset)))))
-
-;; デフォルトではマジックコメントを挿入しない
-(toggle-ruby-magic-comment)
 
 (add-hook 'ruby-mode-hook 'ruby-mode-hooks 'ruby-indent-line)
 (autoload 'ruby-mode "ruby-mode" nil t)
