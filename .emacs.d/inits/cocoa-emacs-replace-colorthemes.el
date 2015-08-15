8c3b1fe3b4adebe5c462a0928f9ef3c5ec96df23
mgi166 <skskoari@gmail.com>
2015-08-15 12:50:32 +0900

    move hi-line-face settings to replace-colorthemes.el

--- .emacs.d/inits/cocoa-emacs-replace-colorthemes.el
+++ .emacs.d/inits/cocoa-emacs-replace-colorthemes.el
@@ -8,3 +8,16 @@
 ;; linum の背景色
 (custom-set-faces
  '(linum ((t (:inherit (shadow default) :background "Gray23")))))
+
+
+;;行をハイライト
+(defface my-hl-line-face
+  '((((class color) (background dark))
+     (:background "Gray25" t))
+    (((class color) (background light))
+     (:background "LightGoldenrodYellow" t))
+    (t (:bold t)))
+  "hl-line's my face")
+(setq hl-line-face 'my-hl-line-face)
+(global-hl-line-mode t)
+
