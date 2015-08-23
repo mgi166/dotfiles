204c3bc258b974e44fe66fabb523f2c204e3cadc
mgi166 <skskoari@gmail.com>
2015-08-23 19:47:34 +0900

    Comment `initchart`

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -27,9 +27,9 @@
 (pallet-mode t)
 
 ;; initchart
-(require 'initchart)
-(initchart-record-execution-time-of load file)
-(initchart-record-execution-time-of require feature)
+;; (require 'initchart)
+;; (initchart-record-execution-time-of load file)
+;; (initchart-record-execution-time-of require feature)
 
 ;; init-loader.el
 (require 'init-loader)
