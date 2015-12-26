6e9d352bebd07fa5bdc8b8696c4025cdb84d8e09
mgi166 <skskoari@gmail.com>
2015-12-27 00:13:10 +0900

    Load env.zsh at first

--- .zsh/all.zsh
+++ .zsh/all.zsh
@@ -1,6 +1,6 @@
+[ -f $HOME/.zsh/env.zsh ]        && source $HOME/.zsh/env.zsh
 [ -f $HOME/.zsh/alias.zsh ]      && source $HOME/.zsh/alias.zsh
 [ -f $HOME/.zsh/color.zsh ]      && source $HOME/.zsh/color.zsh
-[ -f $HOME/.zsh/env.zsh ]        && source $HOME/.zsh/env.zsh
 [ -f $HOME/.zsh/completion.zsh ] && source $HOME/.zsh/completion.zsh
 [ -f $HOME/.zsh/function.zsh ]   && source $HOME/.zsh/function.zsh
 [ -f $HOME/.zsh/history.zsh ]    && source $HOME/.zsh/history.zsh
