b627b042cd3a512591d3a2dc208145923192d9c7
mgi166 <skskoari@gmail.com>
2016-06-21 08:29:34 +0900

    Use `wgrep-ag` in ag-mode.

--- .emacs.d/inits/28_wgrep.el
+++ .emacs.d/inits/28_wgrep.el
@@ -1,8 +1,10 @@
 (require 'wgrep)
 (require 'wgrep-helm)
+(require 'ag)
+(require 'wgrep-ag)
 
 (setq wgrep-auto-save-buffer t)
 (setq wgrep-enable-key "r")
 (autoload 'wgrep-ag-setup "wgrep-ag")
 (add-hook 'ag-mode-hook 'wgrep-ag-setup)
-;(define-key ag-mode-map (kbd "r") 'wgrep-change-to-wgrep-mode)
+(define-key ag-mode-map (kbd "r") 'wgrep-change-to-wgrep-mode)
