99d0dbc64264f815e0458b21b42892b8a91b0536
mgi166 <skskoari@gmail.com>
2016-07-22 15:15:32 +0900

    Set ag-group-mathches to nil.
    * Fix to no work `wgrep-ag`.

--- .emacs.d/inits/28_wgrep.el
+++ .emacs.d/inits/28_wgrep.el
@@ -8,3 +8,6 @@
 (autoload 'wgrep-ag-setup "wgrep-ag")
 (add-hook 'ag-mode-hook 'wgrep-ag-setup)
 (define-key ag-mode-map (kbd "r") 'wgrep-change-to-wgrep-mode)
+
+;; http://qiita.com/koshigoe/items/eadf026fbfc3a704d63d
+(setq ag-group-matches nil)
