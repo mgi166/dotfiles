9d8040106f3d2cadfdb887c1c347d30d4a4a5a00
mgi166 <skskoari@gmail.com>
2017-01-15 08:42:54 +0900

    Add install.sh

---
+++ install.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+set -ex
+
+mitamae_version="1.3.2"
+mitamae_cache="mitamae-${mitamae_version}"
+
+if ! [ -f "bin/${mitamae_cache}" ]; then
+  case "$(uname)" in
+    "Linux")
+      mitamae_bin="mitamae-x86_64-linux"
+      ;;
+    "Darwin")
+      mitamae_bin="mitamae-x86_64-darwin"
+      ;;
+    *)
+      echo "unexpected uname: $(uname)"
+      exit 1
+      ;;
+  esac
+
+  curl -o "bin/${mitamae_bin}.tar.gz" -sL "https://github.com/k0kubun/mitamae/releases/download/v${mitamae_version}/${mitamae_bin}.tar.gz"
+  tar xvzf "bin/${mitamae_bin}.tar.gz"
+  rm "bin/${mitamae_bin}.tar.gz"
+  mv "${mitamae_bin}" "bin/${mitamae_cache}"
+  chmod +x "bin/${mitamae_cache}"
+fi
+
+ln -sf "${mitamae_cache}" bin/mitamae
