ded0e252f3c3d0f5ff330afd3662251f2a816f15
mgi166 <skskoari@gmail.com>
2017-01-15 12:58:44 +0900

    Add cookbooks/tmux.rb

---
+++ cookbooks/tmux.rb
@@ -0,0 +1,12 @@
+link File.expand_path("~/.tmux.conf") do
+  to File.expand_path(".tmux.conf")
+  force true
+end
+
+execute "pip install powerline-status" do
+  not_if "which pyenv > /dev/null && (pip list | grep powerline-status)"
+end
+
+execute "pip install --user git+git://github.com/powerline/powerline" do
+  not_if "which pyenv > /dev/null && which powerline > /dev/null"
+end
