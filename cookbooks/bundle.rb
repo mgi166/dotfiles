622d380befe88407347b1dd10f01d03a573297a0
mgi166 <skskoari@gmail.com>
2017-01-18 23:21:12 +0900

    Create cookbooks/bundle dir.
    Ride the best practice of itamae.

--- cookbooks/bundle.rb
+++
@@ -1,4 +0,0 @@
-link File.expand_path("~/.bundle") do
-  to File.expand_path(".bundle")
-  force true
-end
---
+++ cookbooks/bundle/default.rb
@@ -0,0 +1,4 @@
+link File.expand_path("~/.bundle") do
+  to File.expand_path("cookbooks/bundle/files/.bundle")
+  force true
+end
