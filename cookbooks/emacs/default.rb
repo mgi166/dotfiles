355af7650de0387f42bee5d136f8ba304e1fa793
mgi166 <skskoari@gmail.com>
2017-01-19 23:17:42 +0900

    Update emacs recipe.

--- cookbooks/emacs/default.rb
+++ cookbooks/emacs/default.rb
@@ -1,11 +1,10 @@
 files_path = "cookbooks/emacs/files"
 
 execute "git submodule update --init" do
-  not_if "[[ -e #{files_path}/.emacs.d/elisp/replace-colorthemes ]] && [[ -e #{cookbooks_path}/.emacs.d/site-lisp/jumar ]] && [[ -e #{files_path}/.emacs.d/site-lisp/erfi ]]"
+  not_if "[[ -e #{files_path}/.emacs.d/elisp/replace-colorthemes ]] && [[ -e #{files_path}/.emacs.d/site-lisp/jumar ]] && [[ -e #{files_path}/.emacs.d/site-lisp/erfi ]]"
 end
 
 execute "cask install" do
-  cwd ".emacs.d"
   not_if "[[ -e #{files_path}/.emacs.d/.cask ]]"
 end
 
