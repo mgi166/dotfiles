d270450d89a93789ca31cbc9bc1146c23a03e7df
mgi166 <skskoari@gmail.com>
2017-01-19 23:31:26 +0900

    Set not_if conditions

--- cookbooks/osx_defaults/default.rb
+++ cookbooks/osx_defaults/default.rb
@@ -1,5 +1,20 @@
 # NOTE: Change screenshot settings. See http://ichitaso.com/mac/tips-for-os-x-screenshot/
 #
-execute "defaults write com.apple.screencapture location '~/Pictures/Screenshots'"
-execute "defaults write com.apple.screencapture name 'screenshot'"
-execute "killall SystemUIServer"
+
+location = "~/Pictures/Screenshots"
+
+execute "defaults write com.apple.screencapture location '#{location}'" do
+  not_if "defaults read com.apple.screencapture location | grep '^#{location}$'"
+  notifies :run, 'execute[killall SystemUIServer]'
+end
+
+screenshot_name = "screenshot"
+
+execute "defaults write com.apple.screencapture name '#{screenshot_name}'" do
+  not_if "defaults read com.apple.screencapture name | grep '^#{screenshot_name}$'"
+  notifies :run, 'execute[killall SystemUIServer]'
+end
+
+execute "killall SystemUIServer" do
+  action :nothing
+end
