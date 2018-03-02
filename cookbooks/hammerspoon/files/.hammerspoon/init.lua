49eb9bf29d2894c3b67377dd30bbcb5361060293
mgi166 <skskoari@gmail.com>
2018-03-02 15:59:51 +0900

    Enable key-binds

--- cookbooks/hammerspoon/files/.hammerspoon/init.lua
+++ cookbooks/hammerspoon/files/.hammerspoon/init.lua
@@ -1,16 +1,16 @@
 -- NOTE: For sample from  http://qiita.com/naoya@github/items/81027083aeb70b309c14
--- local function keyCode(key, modifiers)
---    modifiers = modifiers or {}
---    return function()
---       hs.eventtap.event.newKeyEvent(modifiers, string.lower(key), true):post()
---       hs.timer.usleep(1000)
---       hs.eventtap.event.newKeyEvent(modifiers, string.lower(key), false):post()
---    end
--- end
+local function keyCode(key, modifiers)
+   modifiers = modifiers or {}
+   return function()
+      hs.eventtap.event.newKeyEvent(modifiers, string.lower(key), true):post()
+      hs.timer.usleep(1000)
+      hs.eventtap.event.newKeyEvent(modifiers, string.lower(key), false):post()
+   end
+end
 
--- local function remapKey(modifiers, key, keyCode)
---    hs.hotkey.bind(modifiers, key, keyCode, nil, keyCode)
--- end
+local function remapKey(modifiers, key, keyCode)
+   hs.hotkey.bind(modifiers, key, keyCode, nil, keyCode)
+end
 
 -- local function disableAllHotkeys()
 --    for k, v in pairs(hs.hotkey.getHotkeys()) do
@@ -41,12 +41,12 @@
 -- appsWatcher:start()
 
 -- カーソル移動
--- remapKey({'ctrl'}, 'f', keyCode('right'))
--- remapKey({'ctrl'}, 'b', keyCode('left'))
--- remapKey({'ctrl'}, 'n', keyCode('down'))
--- remapKey({'ctrl'}, 'p', keyCode('up'))
--- remapKey({'alt'}, 'f', keyCode('right', {'alt'}))
--- remapKey({'alt'}, 'b', keyCode('left', {'alt'}))
+remapKey({'ctrl'}, 'f', keyCode('right'))
+remapKey({'ctrl'}, 'b', keyCode('left'))
+remapKey({'ctrl'}, 'n', keyCode('down'))
+remapKey({'ctrl'}, 'p', keyCode('up'))
+remapKey({'alt'}, 'f', keyCode('right', {'alt'}))
+remapKey({'alt'}, 'b', keyCode('left', {'alt'}))
 
 -- テキスト編集
 -- remapKey({'ctrl'}, 'w', keyCode('x', {'cmd'}))
@@ -56,10 +56,10 @@
 -- remapKey({'ctrl'}, 'g', keyCode('escape'))
 
 -- ページスクロール
--- remapKey({'ctrl'}, 'v', keyCode('pagedown'))
--- remapKey({'alt'}, 'v', keyCode('pageup'))
--- remapKey({'alt', 'shift'}, ',', keyCode('home'))
--- remapKey({'alt', 'shift'}, '.', keyCode('end'))
+remapKey({'ctrl'}, 'v', keyCode('pagedown'))
+remapKey({'alt'}, 'v', keyCode('pageup'))
+remapKey({'alt', 'shift'}, ',', keyCode('home'))
+remapKey({'alt', 'shift'}, '.', keyCode('end'))
 
 -- NOTE: For debug
 -- http://kitak.hatenablog.jp/entry/2016/11/28/104038
