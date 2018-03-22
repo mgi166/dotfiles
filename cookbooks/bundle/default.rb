b036ffcb7837f13e469821c76c47e422fd0e8889
mgi166 <skskoari@gmail.com>
2018-03-22 20:35:53 +0900

    Comment out nokogiri configuration in bundle

--- cookbooks/bundle/default.rb
+++ cookbooks/bundle/default.rb
@@ -2,3 +2,9 @@ link File.expand_path("~/.bundle") do
   to File.expand_path("cookbooks/bundle/files/.bundle")
   force true
 end
+
+# NOTE: Require libxml2
+# execute 'Set nokogiri build configuration' do
+#   command "bundle config build.nokogiri --use-system-libraries --with-xml2-include=$(brew --prefix libxml2)/include/libxml2"
+#   not_if "bundle config build.nokogiri | grep use-system-libraries"
+# end
