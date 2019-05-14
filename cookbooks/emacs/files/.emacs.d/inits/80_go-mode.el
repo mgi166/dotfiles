3d9d9d56e3d7471abf11ad978e51ec195e58b843
mgi166 <skskoari@gmail.com>
2019-05-14 23:05:29 +0900

    Update go-mode

--- cookbooks/emacs/files/.emacs.d/inits/80_go-mode.el
+++ cookbooks/emacs/files/.emacs.d/inits/80_go-mode.el
@@ -3,11 +3,21 @@
 (eval-after-load "go-mode"
   '(progn
      (require 'go-autocomplete)
+     (require 'company-go)
 
 ; NOTE: $ go get -u github.com/rogpeppe/godef
      (define-key go-mode-map (kbd "M-.") 'godef-jump)
-     (define-key go-mode-map (kbd "M-,") 'pop-tag-mark)))
+     (define-key go-mode-map (kbd "M-,") 'pop-tag-mark)
+
+     (company-mode)
+     (setq company-idle-delay 0)
+     (setq completion-ignore-case t)
+     (setq company-selection-wrap-around t)))
 
 ; NOTE: $ go get golang.org/x/tools/cmd/goimports
 (setq gofmt-command "goimports")
 (add-hook 'before-save-hook 'gofmt-before-save)
+
+(require 'go-autocomplete)
+(require 'auto-complete-config)
+(ac-config-default)
