ef0bbd6bcfdc24fc7408061da9968a2b9666a093
mgi166 <skskoari@gmail.com>
2019-05-14 23:11:53 +0900

    Use C-n, C-p select next or previous completion

--- cookbooks/emacs/files/.emacs.d/inits/21_auto-complete.el
+++ cookbooks/emacs/files/.emacs.d/inits/21_auto-complete.el
@@ -19,3 +19,8 @@
 (setq partial-complication-mode 1)
 
 (global-auto-complete-mode t)
+
+(add-hook 'auto-complete-mode-hook
+          (lambda ()
+            (define-key ac-completing-map (kbd "C-n") 'ac-next)
+            (define-key ac-completing-map (kbd "C-p") 'ac-previous)))
