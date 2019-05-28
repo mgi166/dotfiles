9ae96fa8423c78e451b6bce0b61bded25b026ce7
mgi166 <skskoari@gmail.com>
2019-05-28 23:57:17 +0900

    Fix kill-emacs command

--- cookbooks/emacs/files/.emacs.d/inits/28_revive.el
+++ cookbooks/emacs/files/.emacs.d/inits/28_revive.el
@@ -4,4 +4,4 @@
 (autoload 'wipe "revive" "Wipe emacs" t)
 (define-key ctl-x-map "F" 'resume)                        ; C-x F で復元
 (define-key ctl-x-map "K" 'wipe)                          ; C-x K で Kill
-(add-hook 'kill-emacs-hook 'save-current-configuration)
+;;(add-hook 'kill-emacs-hook 'save-current-configuration)
