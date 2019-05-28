e024684bc0c1132d1d3112a10bb6bc431cd88f1e
mgi166 <skskoari@gmail.com>
2019-05-28 23:58:25 +0900

    No use mac-key-mode
    Because it can't use in emacs-26.2

--- cookbooks/emacs/files/.emacs.d/inits/21_mac-key-mode.el
+++ cookbooks/emacs/files/.emacs.d/inits/21_mac-key-mode.el
@@ -1,4 +1,4 @@
-(require 'mac-key-mode)
-(mac-key-mode 1)
-(define-key mac-key-mode-map [(alt {)] 'elscreen-previous)
-(define-key mac-key-mode-map [(alt })] 'elscreen-next)
+;; (require 'mac-key-mode)
+;; (mac-key-mode 1)
+;; (define-key mac-key-mode-map [(alt {)] 'elscreen-previous)
+;; (define-key mac-key-mode-map [(alt })] 'elscreen-next)
