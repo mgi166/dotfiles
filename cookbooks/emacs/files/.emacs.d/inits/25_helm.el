36209aa04423bec64c3ebd9bce456a4cc9ca8775
mgi166 <skskoari@gmail.com>
2019-06-01 11:52:53 +0900

    Update helm-configuration

--- cookbooks/emacs/files/.emacs.d/inits/25_helm.el
+++ cookbooks/emacs/files/.emacs.d/inits/25_helm.el
@@ -6,6 +6,8 @@
 (require 'helm-types)
 (require 'helm-ls-git)
 (require 'helm-descbinds)
+;(require 'helm-projectile)
+(require 'helm-elscreen)
 
 (define-key helm-map (kbd "C-h") 'delete-backward-char)
 (global-set-key (kbd "M-o") 'helm-occur)
@@ -17,3 +19,28 @@
 
 (global-set-key (kbd "M-y") 'helm-show-kill-ring)
 (global-set-key (kbd "M-r") 'helm-resume)
+
+(custom-set-variables
+ '(helm-truncate-lines t)
+ '(helm-mini-default-sources '(helm-source-buffers-list
+                               helm-source-ls-git
+                               helm-source-recentf
+                               helm-elscreen-source-history-list
+                               helm-elscreen-source-list
+                               helm-source-buffer-not-found)))
+
+; http://garaemon.hatenadiary.jp/entry/2018/06/08/005533
+(defun my/helm-mini ()
+   (interactive)
+    (require 'helm-x-files)
+    (unless helm-source-buffers-list
+      (setq helm-source-buffers-list
+            (helm-make-source "Buffers" 'helm-source-buffers)))
+    (setq helm-source-ls-git (helm-ls-git-build-ls-git-source))
+    (helm :sources helm-mini-default-sources
+          :buffer "*helm mini*"
+          :ff-transformer-show-only-basename nil
+          :truncate-lines helm-buffers-truncate-lines))
+
+(global-set-key (kbd "C-x C-b") 'my/helm-mini)
+(global-set-key (kbd "C-x b") 'my/helm-mini)
