2fb0da07aac6f6e8896a713aac96b8b0f41cc78c
mgi166 <skskoari@gmail.com>
2019-06-02 09:45:19 +0900

    Rename

--- cookbooks/emacs/files/.emacs.d/inits/26_string-inflection.el
+++ cookbooks/emacs/files/.emacs.d/inits/26_string-inflection.el
@@ -1,7 +1,7 @@
 (require 'string-inflection)
 
-(defun string-inflection-my-cycle-function (str)
   "foo_bar => fooBar => FOO_BAR => foo_bar"
+(defun my/string-inflection-cycle-function (str)
   (cond
    ((string-inflection-underscore-p str)
     (string-inflection-camelcase-function str))
@@ -13,6 +13,6 @@
 (defun my/string-inflection-my-style-cycle ()
   (interactive)
   (string-inflection-insert
-   (string-inflection-my-cycle-function (string-inflection-get-current-word))))
+   (my/string-inflection-cycle-function (string-inflection-get-current-word))))
 
 (global-set-key (kbd "C-c C-u") 'my/string-inflection-my-style-cycle)
