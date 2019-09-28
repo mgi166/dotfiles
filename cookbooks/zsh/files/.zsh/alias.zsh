da543670759683a365629eb1ab289db6f33a6989
mgi166 <skskoari@gmail.com>
2019-09-28 14:37:49 +0900

    Add ghq alias

--- cookbooks/zsh/files/.zsh/alias.zsh
+++ cookbooks/zsh/files/.zsh/alias.zsh
@@ -41,6 +41,8 @@ alias ga='git add --all'
 alias gp='git pull origin `git rev-parse --abbrev-ref HEAD`'
 alias gpp='git push origin $(git rev-parse --abbrev-ref HEAD)'
 
+alias gh='ghq'
+
 alias s='svn'
 alias ss='svn st'
 alias sc='svn ci'
