2ffdc5b7c6e2c06bd666bb2717cf9509407c0635
mgi166 <skskoari@gmail.com>
2019-10-25 17:36:53 +0900

    Set .gitmessage.txt on provisionning

--- cookbooks/gitconfig/default.rb
+++ cookbooks/gitconfig/default.rb
@@ -2,3 +2,8 @@ link File.expand_path("~/.gitconfig") do
   to File.expand_path("cookbooks/gitconfig/files/.gitconfig")
   force true
 end
+
+link File.expand_path("~/.gitmessage.txt") do
+  to File.expand_path("cookbooks/gitconfig/files/.gitmessage.txt")
+  force true
+end
