5a1e7293c932bb7a3bd010f3f479868bafdb561c
mgi166 <skskoari@gmail.com>
2020-06-06 10:05:04 +0900

    Fix file path

--- cookbooks/alacritty/default.rb
+++ cookbooks/alacritty/default.rb
@@ -1,4 +1,4 @@
 link File.expand_path("~/.alacritty.yml") do
-  to File.expand_path("cookbooks/bundle/files/.alacritty.yml")
+  to File.expand_path("cookbooks/alacritty/files/.alacritty.yml")
   force true
 end
