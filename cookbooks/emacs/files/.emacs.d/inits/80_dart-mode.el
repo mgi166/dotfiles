a4eaa742c69af297575cf2870a79fb3e97f431ee
mgi166 <skskoari@gmail.com>
2020-06-06 12:30:21 +0900

    Install dart-mode

---
+++ cookbooks/emacs/files/.emacs.d/inits/80_dart-mode.el
@@ -0,0 +1,7 @@
+(setq dart-enable-analysis-server t)
+(setq dart-format-on-save t)
+
+(define-key dart-mode-map (kbd "M-.") 'dart-goto)
+(define-key dart-mode-map (kbd "M-,") 'dart-find-refs)
+
+; (add-hook 'dart-mode-hook 'flycheck-mode)
