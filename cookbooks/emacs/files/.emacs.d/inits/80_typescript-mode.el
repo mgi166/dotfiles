a454c35ca149dc2dd95a1259fecf17f9336cc44f
mgi166 <skskoari@gmail.com>
2020-06-07 16:13:28 +0900

    Add typescript-mode

---
+++ cookbooks/emacs/files/.emacs.d/inits/80_typescript-mode.el
@@ -0,0 +1,6 @@
+(add-to-list 'auto-mode-alist '("\\.ts\\'" . typescript-mode))
+(add-hook 'typescript-mode-hook
+          (lambda ()
+            (tide-setup)
+            (eldoc-mode t)
+            (company-mode-on)))
