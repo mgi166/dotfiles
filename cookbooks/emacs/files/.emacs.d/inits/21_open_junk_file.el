e84ae2cfeac3a6f079d69c67acef5aee02c634d3
mgi166 <hiromu.mogi@gmail.com>
2020-12-31 17:53:28 +0900

    Refactor. Use `use-package`

--- cookbooks/emacs/files/.emacs.d/inits/21_open_junk_file.el
+++ cookbooks/emacs/files/.emacs.d/inits/21_open_junk_file.el
@@ -1,2 +1,3 @@
-(require 'open-junk-file)
-(setq open-junk-file-format "/var/tmp/junk/%Y/%m/%d/%H-%M-%S.")
+(use-package open-junk-file
+  :ensure t
+  :init (setq open-junk-file-format "/var/tmp/junk/%Y/%m/%d/%H-%M-%S."))
