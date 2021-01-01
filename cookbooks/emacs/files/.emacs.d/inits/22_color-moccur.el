5d121018aa33fe1a647863cf0c95f20937df9abb
mgi166 <hiromu.mogi@gmail.com>
2021-01-02 00:14:54 +0900

    Separate S-expression

--- cookbooks/emacs/files/.emacs.d/inits/22_color-moccur.el
+++ cookbooks/emacs/files/.emacs.d/inits/22_color-moccur.el
@@ -1,7 +1,7 @@
 (use-package color-moccur
   :init (setq moccur-split-word t)
-  :config ((add-to-list 'dmoccur-exclusion-mask "\\.DS_Store")
-           (add-to-list 'dmoccur-exclusion-mask "^#.+#$")))
+  :config (add-to-list 'dmoccur-exclusion-mask "\\.DS_Store")
+          (add-to-list 'dmoccur-exclusion-mask "^#.+#$"))
 
 ;(when (require 'migemo nil t) ; migemo が使えるときは migemo を使う
 ;  (setq moccur-use-migemo t))
