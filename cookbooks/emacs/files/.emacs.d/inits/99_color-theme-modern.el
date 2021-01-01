53a74ca9cfb8e06f9f3f7b92288bd917c357c623
mgi166 <hiromu.mogi@gmail.com>
2021-01-02 00:24:33 +0900

    Adjust line highlight color

--- cookbooks/emacs/files/.emacs.d/inits/99_color-theme-modern.el
+++ cookbooks/emacs/files/.emacs.d/inits/99_color-theme-modern.el
@@ -1,13 +1,24 @@
-(load-theme 'charcoal-black t t)
-(enable-theme 'charcoal-black)
+(use-package color-theme-modern
+  :ensure t
+  :config (load-theme 'charcoal-black t t)
+          (enable-theme 'charcoal-black)
 
-;;行をハイライト
-(defface my-hl-line-face
-  '((((class color) (background dark))
-     (:background "Gray25" t))
-    (((class color) (background light))
-     (:background "LightGoldenrodYellow" t))
-    (t (:bold t)))
-  "hl-line's my face")
-(setq hl-line-face 'my-hl-line-face)
-(global-hl-line-mode t))))
+          ;;行をハイライト
+          ;; linum の背景色
+          (custom-set-faces
+           '(linum ((t (:inherit (shadow default) :background "Gray23")))))
+
+          (set-face-attribute 'fringe nil :background "Gray18")
+          (set-face-background 'git-gutter:modified "Gray18")
+          (set-face-background 'git-gutter:added "Gray18")
+          (set-face-background 'git-gutter:deleted "Gray18")
+
+          (defface my-hl-line-face
+            '((((class color) (background dark))
+               (:background "Gray25" t))
+              (((class color) (background light))
+               (:background "LightGoldenrodYellow" t))
+              (t (:bold t)))
+            "hl-line's my face")
+          (setq hl-line-face 'my-hl-line-face)
+          (global-hl-line-mode t))
