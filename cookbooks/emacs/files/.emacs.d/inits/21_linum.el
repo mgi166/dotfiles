438b28a5ca66f12ad9952bdcec560ec6f890305b
mgi166 <hiromu.mogi@gmail.com>
2021-01-02 00:13:22 +0900

    Refactor. use `use-package` in linum

--- cookbooks/emacs/files/.emacs.d/inits/21_linum.el
+++ cookbooks/emacs/files/.emacs.d/inits/21_linum.el
@@ -1,8 +1,8 @@
 ;; linum.el(左側に行数表示)
-(require 'linum)
-(setq linum-delay t)
-(defadvice linum-schedule (around my-linum-schedule () activate)
-  (run-with-idle-timer 0.2 nil #'linum-update-current))
-
-(global-linum-mode t)
-(setq linum-format "%5d ")
+(use-package linum
+  :ensure t
+  :config (global-linum-mode t)
+          (setq linum-delay t)
+          (setq linum-format "%5d ")
+          (defadvice linum-schedule (around my-linum-schedule () activate)
+            (run-with-idle-timer 0.2 nil #'linum-update-current)))
