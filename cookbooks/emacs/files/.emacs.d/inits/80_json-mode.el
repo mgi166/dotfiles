c5aa237fc74d4426f8503986b702c675d5d9dcbf
mgi166 <hiromu.mogi@gmail.com>
2021-01-02 18:32:44 +0900

    Refactor. use `use-package` in json-mode

--- cookbooks/emacs/files/.emacs.d/inits/80_json-mode.el
+++ cookbooks/emacs/files/.emacs.d/inits/80_json-mode.el
@@ -1,4 +1,4 @@
-(add-hook 'json-mode-hook
-          (lambda ()
-            (make-local-variable 'js-indent-level)
-            (setq js-indent-level 2)))
+(use-package json-mode
+  :ensure t
+  :init (make-local-variable 'js-indent-level)
+        (setq js-indent-level 2))
