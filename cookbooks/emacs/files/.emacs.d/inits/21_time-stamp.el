2e79df48951a5c2f75271adf00bc5a475e002147
mgi166 <hiromu.mogi@gmail.com>
2021-01-02 16:26:19 +0900

    Refactor. use `use-package` in time-stamp

--- cookbooks/emacs/files/.emacs.d/inits/21_time-stamp.el
+++ cookbooks/emacs/files/.emacs.d/inits/21_time-stamp.el
@@ -1,7 +1,8 @@
 ;;ファイル更新日を自動的に書き換える
-(require 'time-stamp)
-(add-hook 'before-save-hook 'time-stamp)
-(setq time-stamp-active t)
-(setq time-stamp-start "last updated : 2014/05/052014/05/05")
-(setq time-stamp-format "%04y/%02m/%02d")
-(setq time-stamp-end "\\|$")
+(use-package time-stamp
+  :ensure t
+  :init (setq time-stamp-active t)
+        (setq time-stamp-start "last updated : 2014/05/052014/05/052021/01/022021/01/02")
+        (setq time-stamp-format "%04y/%02m/%02d")
+        (setq time-stamp-end "\\|$")
+        (add-hook 'before-save-hook 'time-stamp))
