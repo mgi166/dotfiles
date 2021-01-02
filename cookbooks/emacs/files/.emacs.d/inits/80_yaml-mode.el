04a85ffbbf631d5d02ef33c7b430c390e5ad2922
mgi166 <hiromu.mogi@gmail.com>
2021-01-02 18:32:30 +0900

    :ensure t

--- cookbooks/emacs/files/.emacs.d/inits/80_yaml-mode.el
+++ cookbooks/emacs/files/.emacs.d/inits/80_yaml-mode.el
@@ -1,4 +1,5 @@
 (use-package yaml-mode
+  :ensure t
   :mode (("\\.dig$" . yaml-mode))
   :init (add-hook 'yaml-mode-hook 'highlight-indent-guides-mode)
   :config (setq highlight-indent-guides-method 'bitmap))
