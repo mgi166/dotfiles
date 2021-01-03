6a826b8c13988e8b6994fb5ef3e80f08263c7dde
mgi166 <hiromu.mogi@gmail.com>
2021-01-03 15:54:56 +0900

    Refactor. use `use-package` in hl-line package

--- cookbooks/emacs/files/.emacs.d/inits/21_hi-line.el
+++ cookbooks/emacs/files/.emacs.d/inits/21_hi-line.el
@@ -1,8 +1,9 @@
-(require 'hl-line)
-(defun global-hl-line-timer-function ()
-  (global-hl-line-unhighlight-all)
-  (let ((global-hl-line-mode t))
-    (global-hl-line-highlight)))
-(setq global-hl-line-timer
-      (run-with-idle-timer 0.01 t 'global-hl-line-timer-function))
+(use-package hl-line
+  :init (defun global-hl-line-timer-function ()
+          (global-hl-line-unhighlight-all)
+          (let ((global-hl-line-mode t))
+            (global-hl-line-highlight)))
+        (setq global-hl-line-timer
+              (run-with-idle-timer 0.01 t 'global-hl-line-timer-function)))
+
 ;; (cancel-timer global-hl-line-timer)
