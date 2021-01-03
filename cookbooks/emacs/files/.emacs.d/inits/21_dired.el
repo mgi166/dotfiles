2addc8d2e3ce8f4c296c5a21c365463bb8a10aeb
mgi166 <hiromu.mogi@gmail.com>
2021-01-03 14:26:05 +0900

    Rename file

--- cookbooks/emacs/files/.emacs.d/inits/21_dired.el
+++ cookbooks/emacs/files/.emacs.d/inits/21_dired.el
@@ -1,6 +1,10 @@
 ;; wdired (dired 中に r を押すと、file を rename してくれる)
 (use-package dired
-  :bind ("C-x C-d" . dired))
+  :bind ("C-x C-d" . dired)
+        (:map dired-mode-map
+              ("k" . dired-previous-line)
+              ("j" . dired-next-line)))
+
 (use-package wdired
   :ensure t
   :bind (:map dired-mode-map
