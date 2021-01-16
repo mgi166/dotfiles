c6e2261f16a7d1694bab7cb7f42e32dd281d8735
mgi166 <hiromu.mogi@gmail.com>
2021-01-16 22:28:58 +0900

    Refactor. Use use-package

--- cookbooks/emacs/files/.emacs.d/inits/26_jumar.el
+++ cookbooks/emacs/files/.emacs.d/inits/26_jumar.el
@@ -1,12 +1,11 @@
-(require 'jumar)
-(require 'jumar-dwim)
-(require 'helm)
-
-(jumar-dwim-use-preconfigured-scheme 'list+history)
-(jumar-init)
-
-(define-key global-map (kbd "C-x m") 'jumar-dwim-add-marker)
-(define-key global-map (kbd "C-x j") 'jumar-dwim-jump-current)
-(define-key global-map (kbd "C-x ,") 'jumar-dwim-jump-backward)
-(define-key global-map (kbd "C-x .") 'jumar-dwim-jump-forward)
-(define-key global-map (kbd "C-c m") 'helm-jumar-dwim-jumarkers)
+(use-package jumar)
+(use-package jumar-dwin
+  :config
+  (jumar-dwim-use-preconfigured-scheme 'list+history)
+  (jumar-init)
+  :bind
+  ("C-x m" . jumar-dwim-add-marker)
+  ("C-x j" . jumar-dwim-jump-current)
+  ("C-x ," . jumar-dwim-jump-backward)
+  ("C-x ." . jumar-dwim-jump-forward)
+  ("C-c m" . helm-jumar-dwim-jumarkers))
