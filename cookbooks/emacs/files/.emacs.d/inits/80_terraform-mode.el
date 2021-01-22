d7e5354c2db5b47432d32bfac4526f56fa37d59d
mgi166 <hiromu.mogi@gmail.com>
2021-01-23 00:18:05 +0900

    uncomment

--- cookbooks/emacs/files/.emacs.d/inits/80_terraform-mode.el
+++ cookbooks/emacs/files/.emacs.d/inits/80_terraform-mode.el
@@ -4,9 +4,9 @@
 ;;         (add-hook 'terraform-mode-hook #'terraform-format-on-save-mode)
 ;;   :hook (terraform-mode . #'terraform-format-on-save-mode))
 
-;; (use-package company-terraform
-;;   :ensure t
-;;   :config (company-terraform-init))
+(use-package company-terraform
+  :ensure t
+  :config (company-terraform-init))
 
 (require 'terraform-mode)
 (add-hook 'terraform-mode-hook 'terraform-format-on-save-mode)
