389f7aa5a67beda2ab8fc79717338c8f115720a8
mgi166 <hiromu.mogi@gmail.com>
2021-01-23 10:00:01 +0900

    Refactor. use `use-package` in yasnippets-mode

--- cookbooks/emacs/files/.emacs.d/inits/28_yasnippet.el
+++ cookbooks/emacs/files/.emacs.d/inits/28_yasnippet.el
@@ -1,23 +1,17 @@
-(require 'yasnippet)
-(setq yas-snippet-dirs nil)
-(setq yas-snippet-dirs '("~/.emacs.d/snippets"))
-(yas-global-mode 1)
-
-;; 既存スニペットを挿入する
-(define-key yas-minor-mode-map (kbd "C-x y i") 'yas-insert-snippet)
-;; 新規スニペットを作成するバッファを用意する
-(define-key yas-minor-mode-map (kbd "C-x y n") 'yas-new-snippet)
-;; 既存スニペットを閲覧・編集する
-(define-key yas-minor-mode-map (kbd "C-x y v") 'yas-visit-snippet-file)
-
-(define-key yas-minor-mode-map (kbd "M-i") 'yas/expand)
-
-(defun toggle-yas-mode ()
-  (interactive)
-  (yas-global-mode 'toggle)
-  (message "Toggle yas-mode"))
-
-;; yas-mode を toggle する
-(global-set-key (kbd "C-x y m") 'toggle-yas-mode)
-
-
+(use-package yasnippet
+  :init (setq yas-snippet-dirs nil)
+        (setq yas-snippet-dirs '("~/.emacs.d/snippets"))
+        (defun toggle-yas-mode ()
+          (interactive)
+          (yas-global-mode 'toggle)
+          (message "Toggle yas-mode"))
+  :config (yas-global-mode 1)
+  :bind ("C-x y m" . toggle-yas-mode) ;; yas-mode を toggle する
+        (:map yas-minor-mode-map
+              ;; 既存スニペットを挿入する
+              ("C-x y i" . yas-insert-snippet)
+              ;; 新規スニペットを作成するバッファを用意する
+              ("C-x y n" . yas-new-snippet)
+              ;; 既存スニペットを閲覧・編集する
+              ("C-x y v" . yas-visit-snippet-file)
+              ("M-i" . yas/expand)))
