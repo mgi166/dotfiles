d5e3bb5472fe306545215acd33708b3c226ef18b
mgi166 <hiromu.mogi@gmail.com>
2021-01-23 09:54:18 +0900

    More recentf items

--- cookbooks/emacs/files/.emacs.d/inits/26_recentf.el
+++ cookbooks/emacs/files/.emacs.d/inits/26_recentf.el
@@ -1,8 +1,8 @@
 ;; recentf.el
 ;; 最近使ったファイルをメニューに表示
 (use-package recentf
-  :init (setq recentf-max-menu-items 200)
-        (setq recentf-max-saved-items 200)
+  :init (setq recentf-max-menu-items 400)
+        (setq recentf-max-saved-items 400)
         (setq recentf-exclude '("/recentf" "COMMIT_EDITMSG" ".?TAG"))
   :bind ("C-x f" . recentf-open-files)
   :config (recentf-mode 1))
