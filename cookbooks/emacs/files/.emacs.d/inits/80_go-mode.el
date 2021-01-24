7b22cbde4699508a2ab9470ad5a14c569a556655
mgi166 <hiromu.mogi@gmail.com>
2021-01-24 10:05:41 +0900

    Refactor. use `use-package` in go-mode

--- cookbooks/emacs/files/.emacs.d/inits/80_go-mode.el
+++ cookbooks/emacs/files/.emacs.d/inits/80_go-mode.el
@@ -1,26 +1,15 @@
-; NOTE: require
-; go get -u github.com/nsf/gocode
-(eval-after-load "go-mode"
-  '(progn
-     (require 'go-autocomplete)
-     (ac-config-default)
-     (require 'company-go)
+(use-package go-mode
+  ; NOTE: $ go get golang.org/x/tools/cmd/goimports
+  :init (subword-mode 1)
+        (setq gofmt-command "goimports")
+  :config (use-package go-autocomplete)
+          (ac-config-default)
+          (use-package company-go)
+          (use-package go-eldoc)
+  :hook (go-mode . #'go-eldoc-setup)
+        (go-mode . #'gofmt-before-save)
+  ; NOTE: $ go get -u github.com/rogpeppe/godef
+  :bind
+  (:map go-mode-map ("M-." . godef-jump)
+                    ("M-," . pop-tag-mark)))
 
-     ; NOTE: $ go get -u github.com/rogpeppe/godef
-     (define-key go-mode-map (kbd "M-.") 'godef-jump)
-     (define-key go-mode-map (kbd "M-,") 'pop-tag-mark)
-
-     (require 'go-eldoc)
-     (add-hook 'go-mode-hook 'go-eldoc-setup)
-
-     (company-mode)
-     (setq company-idle-delay 0)
-     (setq completion-ignore-case t)
-     (setq company-selection-wrap-around t)))
-
-; NOTE: $ go get golang.org/x/tools/cmd/goimports
-(setq gofmt-command "goimports")
-(add-hook 'before-save-hook 'gofmt-before-save)
-
-(add-hook 'go-mode-hook (lambda ()
-                          (subword-mode 1)))
