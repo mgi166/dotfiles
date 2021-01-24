ff5ff3cb1bbdedfc7d577bc93c67333940c108b0
mgi166 <hiromu.mogi@gmail.com>
2021-01-24 14:52:26 +0900

    Use `*-seutp` functions for dap-debug

--- cookbooks/emacs/files/.emacs.d/inits/79_dap-mode.el
+++ cookbooks/emacs/files/.emacs.d/inits/79_dap-mode.el
@@ -1,20 +1,17 @@
 (use-package dap-mode
   :ensure t
   :after lsp-mode
-  :custom
-  (dap-auto-configure-features '(sessions locals breakpoints expressions repl controls tooltip))
-  :config
-  (dap-mode 1)
-  (dap-auto-configure-mode 1)
-  ;(require 'dap-hydra)
-  (require 'dap-go)
-  ;; Install goDebug.js to ~/.emacs.d/.extension/vscode/golang.go/extension/out/src/debugAdapter/
-  (dap-go-setup)
-  (require 'dap-gdb-lldb)
-  ;; Install gdb.js to ~/.emacs.d/.extension/vscode/webfreak.debug/extension/out/src/
-  (dap-gdb-lldb-setup)
-  (require 'dap-lldb)
-  (use-package dap-ui
-      :ensure nil
-      :config
-      (dap-ui-mode 1)))
+  :custom (dap-auto-configure-features '(sessions locals breakpoints expressions repl controls tooltip))
+  :config (dap-mode 1)
+          (dap-auto-configure-mode 1)
+          ;(require 'dap-hydra)
+          (use-package dap-go)
+          ;; Install goDebug.js to ~/.emacs.d/.extension/vscode/golang.go/extension/out/src/debugAdapter/
+          (dap-go-setup)
+          (use-package dap-gdb-lldb)
+          ;; Install gdb.js to ~/.emacs.d/.extension/vscode/webfreak.debug/extension/out/src/
+          (dap-gdb-lldb-setup)
+          (use-package dap-lldb)
+          (use-package dap-ui
+              :ensure nil
+              :config (dap-ui-mode 1)))
