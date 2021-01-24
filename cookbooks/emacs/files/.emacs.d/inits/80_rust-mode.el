ad96284355015fd84167b763587f0f9612fa6fee
mgi166 <hiromu.mogi@gmail.com>
2021-01-24 10:04:24 +0900

    wip. Use rust-mode debug-template

--- cookbooks/emacs/files/.emacs.d/inits/80_rust-mode.el
+++ cookbooks/emacs/files/.emacs.d/inits/80_rust-mode.el
@@ -9,3 +9,19 @@
 (use-package cargo
   :ensure t
   :hook (rust-mode . cargo-minor-mode))
+
+(dap-register-debug-template
+ "LLDB::Run with lldb-vscode"
+  (list :type "lldb"
+        :cmd "~/.cargo/bin/rust-lldb"
+        :request "launch"
+        :program "target/debug/rpn"
+        :miDebuggerPath "~/.vscode/extensions/ms-vscode.cpptools-1.1.3/debugAdapters/lldb-mi/bin/"
+        :name "LLDB::Run"))
+
+(dap-register-debug-template
+ "LLDB::GDB rust-gdb"
+  (list :type "gdb"
+        :request "launch"
+        :name "LLDB::Run"
+        :gdbpath "rust-gdb"))
