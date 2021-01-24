73dd0bf523505cf430ce562af29fb0c19cc1259d
mgi166 <hiromu.mogi@gmail.com>
2021-01-24 12:28:37 +0900

    Set dap-ui buffers

--- cookbooks/emacs/files/.emacs.d/inits/79_dap-mode.el
+++ cookbooks/emacs/files/.emacs.d/inits/79_dap-mode.el
@@ -5,6 +5,13 @@
   :bind (:map dap-mode-map ("<f5>" . dap-debug))
   :config (dap-mode 1)
           (dap-auto-configure-mode 1)
+          (setq dap-ui-buffer-configurations
+                `((,dap-ui--locals-buffer . ((side . left) (slot . 1) (window-width . 0.5)))
+                  (,dap-ui--expressions-buffer . ((side . right) (slot . 2) (window-width . 0.1)))
+                  (,dap-ui--sessions-buffer . ((side . right) (slot . 3) (window-width . 0.20)))
+                  (,dap-ui--breakpoints-buffer . ((side . right) (slot . 1) (window-width . ,treemacs-width)))
+                  (,dap-ui--debug-window-buffer . ((side . bottom) (slot . 1) (window-width . 0.05)))
+                  (,dap-ui--repl-buffer . ((side . left) (slot . 2) (window-width . 0.5)))))
           ;(require 'dap-hydra)
           (use-package dap-go)
           ;; Install goDebug.js to ~/.emacs.d/.extension/vscode/golang.go/extension/out/src/debugAdapter/
