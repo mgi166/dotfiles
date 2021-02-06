4a89db2fbbd9b55d9e13e3854b2f40e348d61ce3
mgi166 <hiromu.mogi@gmail.com>
2021-02-06 10:11:28 +0900

    Combine python-mode to lsp-mode

--- cookbooks/emacs/files/.emacs.d/inits/78_lsp-mode.el
+++ cookbooks/emacs/files/.emacs.d/inits/78_lsp-mode.el
@@ -6,6 +6,7 @@
         (setq read-process-output-max (* 1024 1024)) ;; 1mb
   :hook (rust-mode . lsp)
         ;(go-mode . lsp)
+        (python-mode . lsp)
         (lsp-mode . lsp-enable-which-key-integration)
   :bind ("C-c h" . lsp-describe-thing-at-point)
         (:map lsp-mode-map
