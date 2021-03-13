0a8299df14f35ad9ecedca87c566df24923e0f2c
mgi166 <hiromu.mogi@gmail.com>
2021-03-14 08:31:21 +0900

    use-package typescript mode

--- cookbooks/emacs/files/.emacs.d/inits/80_typescript-mode.el
+++ cookbooks/emacs/files/.emacs.d/inits/80_typescript-mode.el
@@ -1,7 +1,4 @@
-(add-to-list 'auto-mode-alist '("\\.ts\\'" . typescript-mode))
-(add-hook 'typescript-mode-hook
-          (lambda ()
-            ;(tide-setup)
-            (eldoc-mode t)
-            (company-mode-on)
-            (subword-mode 1)))
+(use-package typescript-mode
+  :ensure t
+  :custom (custom-set-variables '(typescript-indent-level 2))
+  :config (subword-mode 1))
