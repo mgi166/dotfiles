0fd7bcf11025e7973e8849a6ab9a16e04d8845c6
mgi166 <hiromu.mogi@gmail.com>
2021-03-27 13:55:06 +0900

    gotmpl as yaml-mode

--- cookbooks/emacs/files/.emacs.d/inits/80_yaml-mode.el
+++ cookbooks/emacs/files/.emacs.d/inits/80_yaml-mode.el
@@ -1,5 +1,6 @@
 (use-package yaml-mode
   :ensure t
-  :mode (("\\.dig$" . yaml-mode))
+  :mode (("\\.dig$" . yaml-mode)
+         ("\\.gotmpl$" . yaml-mode))
   :init (add-hook 'yaml-mode-hook 'highlight-indent-guides-mode)
   :config (setq highlight-indent-guides-method 'column))
