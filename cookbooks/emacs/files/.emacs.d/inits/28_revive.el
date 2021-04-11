4d42fab00f76ee3d68672e64890611c03b7e1272
mgi166 <hiromu.mogi@gmail.com>
2021-04-11 16:15:10 +0900

    Remove revive package. It doesn't use

--- cookbooks/emacs/files/.emacs.d/inits/28_revive.el
+++
@@ -1,7 +0,0 @@
-;; revive.el (前回のバッファをsaveし、load する)
-(autoload 'save-current-configuration "revive" "Save status" t)
-(autoload 'resume "revive" "Resume Emacs" t)
-(autoload 'wipe "revive" "Wipe emacs" t)
-(define-key ctl-x-map "F" 'resume)                        ; C-x F で復元
-(define-key ctl-x-map "K" 'wipe)                          ; C-x K で Kill
-;;(add-hook 'kill-emacs-hook 'save-current-configuration)
