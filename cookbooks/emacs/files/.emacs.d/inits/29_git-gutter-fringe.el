c1bd8cb08207c32d1ca762a3435227383059852f
mgi166 <hiromu.mogi@gmail.com>
2021-04-11 15:17:00 +0900

    Refactor. Use `use-package` syntax

--- cookbooks/emacs/files/.emacs.d/inits/29_git-gutter-fringe.el
+++ cookbooks/emacs/files/.emacs.d/inits/29_git-gutter-fringe.el
@@ -1,11 +1,10 @@
-(require 'git-gutter-fringe)
-(custom-set-variables
- '(git-gutter:update-interval 2))
-(setq-default left-fringe-width  18)
-(setq-default right-fringe-width 18)
-(custom-set-variables
- '(git-gutter:modified-sign "="))
-(global-git-gutter-mode)
-
-(global-set-key (kbd "C-c r") 'git-gutter:revert-hunk)
-(global-set-key (kbd "C-c s") 'git-gutter:stage-hunk)
+(use-package git-gutter-fringe
+  :custom (setq-default left-fringe-width  18)
+          (setq-default right-fringe-width 18)
+          (custom-set-variables
+            '(git-gutter:modified-sign "="))
+          (custom-set-variables
+           '(git-gutter:update-interval 2))
+  :bind ("C-c r" . 'git-gutter:revert-hunk)
+        ("C-c s" . 'git-gutter:stage-hunk)
+  :config (global-git-gutter-mode))
