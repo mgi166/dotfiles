4f0e9f49c4f529883640c120361304936985398e
mgi166 <hiromu.mogi@gmail.com>
2021-04-17 14:12:36 +0900

    Refactor. Use `use-package` in terraform

--- cookbooks/emacs/files/.emacs.d/inits/80_terraform-mode.el
+++ cookbooks/emacs/files/.emacs.d/inits/80_terraform-mode.el
@@ -1,19 +1,10 @@
-;; (use-package terraform-mode
-;;   :ensure t
-;;   :init (custom-set-variables '(terraform-indent-level 2))
-;;         (add-hook 'terraform-mode-hook #'terraform-format-on-save-mode)
-;;   :hook (terraform-mode . #'terraform-format-on-save-mode))
+(use-package terraform-mode
+  :ensure t
+  :hook (terraform-mode . company-mode)
+        (before-save . terraform-format-on-save-mode)
+  :custom (custom-set-variables '(terraform-indent-level 2))
+  :init (add-hook 'terraform-mode-hook 'terraform-format-on-save-mode))
 
 (use-package company-terraform
   :ensure t
   :config (company-terraform-init))
-
-(require 'terraform-mode)
-(add-hook 'terraform-mode-hook 'terraform-format-on-save-mode)
-
-;; (custom-set-variables
-;;  '(terraform-indent-level 2))
-
-;; (require 'company-terraform)
-;; (company-terraform-init)
-;; (add-hook 'terraform-mode-hook 'terraform-format-on-save-mode)
