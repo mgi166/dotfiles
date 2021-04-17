eea44b344e8690066945d1450ea91c78986819dd
mgi166 <hiromu.mogi@gmail.com>
2021-04-17 14:05:50 +0900

    Refactor. Use `use-package` in helm

--- cookbooks/emacs/files/.emacs.d/inits/25_helm.el
+++ cookbooks/emacs/files/.emacs.d/inits/25_helm.el
@@ -1,5 +1,36 @@
+; http://garaemon.hatenadiary.jp/entry/2018/06/08/005533
+(defun my/helm-mini ()
+   (interactive)
+    (require 'helm-x-files)
+    (unless helm-source-buffers-list
+      (setq helm-source-buffers-list
+            (helm-make-source "Buffers" 'helm-source-buffers)))
+    (setq helm-source-ls-git (helm-ls-git-build-ls-git-source))
+    (helm :sources helm-mini-default-sources
+          :buffer "*helm mini*"
+          :ff-transformer-show-only-basename nil
+          :truncate-lines helm-buffers-truncate-lines))
+
 (use-package helm
-  :ensure t)
+  :ensure t
+  :custom (custom-set-variables
+           '(helm-truncate-lines t)
+           '(helm-mini-default-sources '(helm-source-buffers-list
+                                         helm-source-ls-git
+                                         helm-source-recentf
+                                         helm-elscreen-source-history-list
+                                         helm-elscreen-source-list
+                                         helm-source-buffer-not-found)))
+  :bind ("M-o" . helm-occur)
+        ("C-;" . 'helm-M-x)
+        ("C-c C-b" . helm-ls-git-ls)
+        ("C-c d" . helm-browse-project)
+        ("C-c C-d" . helm-browse-project)
+        ("M-y" . helm-show-kill-ring)
+        ("M-r" . helm-resume)
+        ("C-x C-b" . 'my/helm-mini)
+        ("C-x b" . 'my/helm-mini)
+        (:map helm-map ("C-h" . delete-backward-char)))
 
 (use-package helm-config)
 (use-package vc)
@@ -15,39 +46,3 @@
 
 (use-package helm-elscreen
   :ensure t)
-
-(define-key helm-map (kbd "C-h") 'delete-backward-char)
-(global-set-key (kbd "M-o") 'helm-occur)
-(global-set-key (kbd "C-;") 'helm-M-x)
-
-(global-set-key (kbd "C-c C-b") 'helm-ls-git-ls)
-(global-set-key (kbd "C-c d") 'helm-browse-project)
-(global-set-key (kbd "C-c C-d") 'helm-browse-project)
-
-(global-set-key (kbd "M-y") 'helm-show-kill-ring)
-(global-set-key (kbd "M-r") 'helm-resume)
-
-(custom-set-variables
- '(helm-truncate-lines t)
- '(helm-mini-default-sources '(helm-source-buffers-list
-                               helm-source-ls-git
-                               helm-source-recentf
-                               helm-elscreen-source-history-list
-                               helm-elscreen-source-list
-                               helm-source-buffer-not-found)))
-
-; http://garaemon.hatenadiary.jp/entry/2018/06/08/005533
-(defun my/helm-mini ()
-   (interactive)
-    (require 'helm-x-files)
-    (unless helm-source-buffers-list
-      (setq helm-source-buffers-list
-            (helm-make-source "Buffers" 'helm-source-buffers)))
-    (setq helm-source-ls-git (helm-ls-git-build-ls-git-source))
-    (helm :sources helm-mini-default-sources
-          :buffer "*helm mini*"
-          :ff-transformer-show-only-basename nil
-          :truncate-lines helm-buffers-truncate-lines))
-
-(global-set-key (kbd "C-x C-b") 'my/helm-mini)
-(global-set-key (kbd "C-x b") 'my/helm-mini)
