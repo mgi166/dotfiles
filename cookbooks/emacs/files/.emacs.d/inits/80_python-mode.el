db169ac55ed1439a2707bd2ca017f22845cb7cbc
mgi166 <hiromu.mogi@gmail.com>
2021-05-03 15:14:25 +0900

    Not use lsp-jedi. Use company-jedi

--- cookbooks/emacs/files/.emacs.d/inits/80_python-mode.el
+++ cookbooks/emacs/files/.emacs.d/inits/80_python-mode.el
@@ -1,10 +1,15 @@
 (use-package python-mode
   :ensure t
   :init (add-hook 'python-mode-hook 'highlight-indent-guides-mode)
-  :config (setq highlight-indent-guides-method 'column))
+  :config (setq highlight-indent-guides-method 'column)
+  (setq jedi:server-command (list (executable-find "jediepcserver")))
+  (add-to-list 'company-backends 'company-jedi))
 
-(use-package lsp-jedi
-  :ensure t
-  :config (with-eval-after-load "lsp-mode"
-            (add-to-list 'lsp-disabled-clients 'pyls)
-            (add-to-list 'lsp-enabled-clients 'jedi)))
+;; (use-package lsp-jedi
+;;   :ensure t
+;;   :config (with-eval-after-load "lsp-mode"
+;;             (add-to-list 'lsp-disabled-clients 'pyls)
+;;             (add-to-list 'lsp-enabled-clients 'jedi)))
+
+;; (use-package company-jedi
+;;   :ensure t)
