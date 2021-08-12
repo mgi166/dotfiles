58b5f1e85db346aa6e0d9e725e9fd0e4f7edda45
mgi166 <hiromu.mogi@gmail.com>
2021-08-12 23:47:00 +0900

    Add install command awscli@v2

--- cookbooks/aws/default.rb
+++ cookbooks/aws/default.rb
@@ -6,3 +6,12 @@ link File.expand_path("~/.aws/config") do
   to File.expand_path("cookbooks/aws/files/.aws/config")
   force true
 end
+
+# https://docs.aws.amazon.com/ja_jp/cli/latest/userguide/install-cliv2-mac.html
+execute "install awscli@v2" do
+  command <<~EOF
+    curl "https://awscli.amazonaws.com/AWSCLIV2.pkg" -o "AWSCLIV2.pkg"
+    sudo installer -pkg AWSCLIV2.pkg -target /
+  EOF
+  if_not "which aws"
+end
