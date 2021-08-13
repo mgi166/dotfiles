67ed89c852049b9e4e1c637a702347d8b93eced3
mgi166 <hiromu.mogi@gmail.com>
2021-08-13 16:09:40 +0900

    Remove tmux-powerline

--- cookbooks/tmux/default.rb
+++ cookbooks/tmux/default.rb
@@ -4,11 +4,3 @@ link File.expand_path("~/.tmux.conf") do
   to File.expand_path("cookbooks/tmux/files/.tmux.conf")
   force true
 end
-
-execute "pip install powerline-status" do
-  not_if "which pyenv > /dev/null && (pip list | grep powerline-status)"
-end
-
-execute "pip install --user git+git://github.com/powerline/powerline" do
-  not_if "which pyenv > /dev/null && which powerline > /dev/null"
-end
