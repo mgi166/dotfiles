db5e3a97d0672bd74bcf7e6e9fda89d2d8a6c077
mgi166 <hiromu.mogi@gmail.com>
2021-08-13 23:34:07 +0900

    Remove default python install

--- cookbooks/pyenv/default.rb
+++ cookbooks/pyenv/default.rb
@@ -1,13 +1,3 @@
 git File.expand_path("~/.pyenv") do
   repository "https://github.com/yyuu/pyenv.git"
 end
-
-python_version = "3.6.0"
-
-execute "PATH=$HOME/.pyenv/bin:$PATH pyenv install #{python_version}" do
-  not_if "pyenv versions | grep #{python_version}"
-end
-
-execute "PATH=$HOME/.pyenv/bin:$PATH pyenv global #{python_version}" do
-  not_if "pyenv versions | grep '\*' | grep #{python_version}"
-end
