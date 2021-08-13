80e41ee3bc2c8e7a929de9b277c0da2b22dc0f12
mgi166 <hiromu.mogi@gmail.com>
2021-08-13 15:47:36 +0900

    Fix method

--- cookbooks/aws/default.rb
+++ cookbooks/aws/default.rb
@@ -13,5 +13,5 @@ execute "install awscli@v2" do
     curl "https://awscli.amazonaws.com/AWSCLIV2.pkg" -o "AWSCLIV2.pkg"
     sudo installer -pkg AWSCLIV2.pkg -target /
   EOF
-  if_not "which aws"
+  not_if "which aws"
 end
