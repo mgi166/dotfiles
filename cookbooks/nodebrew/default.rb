8d5fe246c4c01a3588b0a61433210da4b4bbb92f
mgi166 <hiromu.mogi@gmail.com>
2021-08-13 23:39:01 +0900

    Remove default node version

--- cookbooks/nodebrew/default.rb
+++ cookbooks/nodebrew/default.rb
@@ -1,13 +1,3 @@
 execute "curl -L git.io/nodebrew | perl - setup" do
   not_if "[[ -e ~/.nodebrew ]]"
 end
-
-node_version = "6.9.4"
-
-execute "PATH=$HOME/.nodebrew/current/bin:$PATH nodebrew install-binary v#{node_version}" do
-  not_if "nodebrew ls | grep '^v#{node_version}$'"
-end
-
-execute "PATH=$HOME/.nodebrew/current/bin:$PATH nodebrew use v#{node_version}" do
-  not_if "nodebrew ls | grep current | grep #{node_version}"
-end
