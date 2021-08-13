0a95c28708ddf7831a905240cfd42a9e6db126f1
mgi166 <hiromu.mogi@gmail.com>
2021-08-14 00:05:00 +0900

    Fix recipe order

--- cookbooks/emacs/default.rb
+++ cookbooks/emacs/default.rb
@@ -4,18 +4,13 @@ package "emacs" do
   options "--cask"
 end
 
-# NOTE: Deprecate `linkapps` command
-execute "brew linkapps emacs" do
-  not_if "[[ -e /Applications/Emacs.app ]]"
-end
-
 package "cask"
-
-execute "cd #{files_path}/.emacs.d; cask install" do
-  not_if "[[ -e #{files_path}/.emacs.d/.cask ]]"
-end
 
 link File.expand_path("~/.emacs.d") do
   to File.expand_path("cookbooks/emacs/files/.emacs.d")
   force true
+end
+
+execute "cd #{files_path}/.emacs.d; cask install" do
+  not_if "[[ -e #{files_path}/.emacs.d/.cask ]]"
 end
