bbbe8e8601d6af5b25f841ea011895b5294dbcbd
mgi166 <hiromu.mogi@gmail.com>
2022-02-19 19:39:29 +0900

    Suspend markdown-preview-mode

--- cookbooks/emacs/files/.emacs.d/inits/80_markdown-mode.el
+++ cookbooks/emacs/files/.emacs.d/inits/80_markdown-mode.el
@@ -1,5 +1,7 @@
 (use-package markdown-mode
   :ensure t
+;  :init (add-hook 'markdown-mode-hook 'markdown-preview-mode)
   :bind (:map markdown-mode-map
               ("M-n" . markdown-forward-paragraph)
-              ("M-p" . markdown-backward-paragraph)))
+              ("M-p" . markdown-backward-paragraph))
+  :config (setq markdown-preview-stylesheets (list "github.css")))
