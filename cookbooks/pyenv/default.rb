b4d4530c252678b19cbae930bc9857136f64faae
mgi166 <hiromu.mogi@gmail.com>
2022-11-07 12:52:23 +0900

    Upgrade pyenv

--- cookbooks/pyenv/default.rb
+++ cookbooks/pyenv/default.rb
@@ -1,3 +1,7 @@
 git File.expand_path("~/.pyenv") do
   repository "https://github.com/pyenv/pyenv.git"
 end
+
+git File.expand_path("~/.pyenv/plugins/pyenv-update") do
+  repository "https://github.com/pyenv/pyenv-update.git"a
+end
