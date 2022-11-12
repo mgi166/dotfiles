39bd3156c09b526510ce0d2dc073663261eb6740
mgi166 <hiromu.mogi@gmail.com>
2022-11-13 00:46:55 +0900

    Only create ~/.config directory in config receipe

--- cookbooks/config/default.rb
+++ cookbooks/config/default.rb
@@ -1,5 +1,6 @@
-# TODO: create ~/.config dir only
-link File.expand_path("~/.config") do
-  to File.expand_path("cookbooks/config/files/.config")
-  force true
-end
+directory File.expand_path("~/.config")
+
+# link File.expand_path("~/.config") do
+#   to File.expand_path("cookbooks/config/files/.config")
+#   force true
+# end
