addb859c8ec38ad381a3391b382ee842916e6b05
mgi166 <hiromu.mogi@gmail.com>
2022-11-28 13:04:24 +0900

    Remove elscreen settings

--- cookbooks/emacs/files/.emacs.d/inits/23_elscreen.el
+++ cookbooks/emacs/files/.emacs.d/inits/23_elscreen.el
@@ -1,58 +1,16 @@
-;; [0, 1, 2] で 1 をkill-screen したときに [0, 1] となるようにする
-;; see(http://d.hatena.ne.jp/asudofu/20091121/1258778536)
-(defun elscreen-insert-internal (screen)
-  (elscreen-clone screen)
-  (elscreen-kill-internal screen))
+(tab-bar-mode 1)
+(defvar ctl-z-map (make-keymap))
+(define-key global-map (kbd "C-z") ctl-z-map)
 
-(defun elscreen-get-gap-next ()
-  (let ((screen-list (sort (elscreen-get-screen-list) '<))
-        (screen 0))
-    (while (eq (nth screen screen-list) screen)
-      (setq screen (+ screen 1)))
-    (nth screen screen-list)))
+(define-key ctl-z-map (kbd "k") 'tab-close)
+(define-key ctl-z-map (kbd "t") 'dired-other-tab)
+(define-key ctl-z-map (kbd "u") 'tab-undo)
+(define-key ctl-z-map (kbd "d") 'dired-other-tab)
+(define-key ctl-z-map (kbd "C-k") 'tab-close)
+(define-key ctl-z-map (kbd "C-c") 'tab-new)
+(define-key ctl-z-map (kbd "C-d") 'dired-other-tab)
+(define-key ctl-z-map (kbd "C-n") 'tab-next)
+(define-key ctl-z-map (kbd "C-p") 'tab-previous)
 
-(defun elscreen-get-packed-num ()
-  (let ((screen-list (sort (elscreen-get-screen-list) '<))
-        (current-screen (elscreen-get-current-screen))
-        (screen 0))
-    (while (not (eq (nth screen screen-list) current-screen))
-      (setq screen (+ screen 1)))
-    screen))
-
-(defun elscreen-pack-list ()
-  (interactive)
-  (let ((next (elscreen-get-gap-next))
-        (pack (elscreen-get-packed-num)))
-    (while next
-      (elscreen-insert-internal next)
-      (setq next (elscreen-get-gap-next)))
-    (elscreen-goto pack)
-    (elscreen-notify-screen-modification 'force)))
-
-;; elscreen.el 非依存版 (バッファをタブ化。http://d.hatena.ne.jp/tam5917/20120922/1348286748)
-(use-package elscreen
-  :ensure t
-  :init (setq elscreen-tab-display-kill-screen nil) ;; [X]を表示しない
-        (setq elscreen-tab-display-control nil)     ;; [<->]を表示しない
-        (add-hook 'elscreen-kill-hook 'elscreen-pack-list) ;; killしたらpackする
-  ;; C-z k or C-z C-k でバッファもkillするように
-  ;; C-z K or C-z C-K で screen のみ kill する
-  :bind ("M-[" . elscreen-previous)
-        ("M-]" . elscreen-next)
-        ("s-{" . elscreen-previous)
-        ("s-}" . elscreen-next)
-        ("“" . elscreen-previous)
-        ("”" . 'elscreen-next)
-        (:map elscreen-map ("C-k" . elscreen-kill-screen-and-buffers)
-                           ("k" . elscreen-kill-screen-and-buffers)
-                           ("K" . elscreen-kill-screen-and-buffers)
-                           ("C-K" . elscreen-kill)
-                           ("C-o" . elscreen-kill-others))
-  :init (elscreen-start))
-
-(if window-system
-  (define-key elscreen-map "\C-z" 'iconify-or-deiconify-frame)
-  (define-key elscreen-map "\C-z" 'suspend-emacs))
-
-;; elscreen-server
-(use-package elscreen-server)
+(define-key global-map (kbd "M-]") 'tab-next)
+(define-key global-map (kbd "M-[") 'tab-previous)
