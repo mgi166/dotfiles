cee85d97d79425f587819d2878501737271ca773
mgi166 <hiromu.mogi@gmail.com>
2022-11-28 13:04:24 +0900

    Upgrade recentf file list
    Acked-by: mgi166 <hiromu.mogi@gmail.com>

--- cookbooks/emacs/files/.emacs.d/inits/26_recentf.el
+++ cookbooks/emacs/files/.emacs.d/inits/26_recentf.el
@@ -1,8 +1,8 @@
 ;; recentf.el
 ;; 最近使ったファイルをメニューに表示
 (use-package recentf
-  :init (setq recentf-max-menu-items 400)
-        (setq recentf-max-saved-items 400)
+  :init (setq recentf-max-menu-items 1000)
+        (setq recentf-max-saved-items 1000)
         (setq recentf-exclude '("/recentf" "COMMIT_EDITMSG" ".?TAG"))
   :bind ("C-x f" . recentf-open-files)
   :config (recentf-mode 1))
