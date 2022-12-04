24431d99333ff09fd010c1ac424a1ddcde3d194e
mgi166 <hiromu.mogi@gmail.com>
2022-12-04 13:04:24 +0900

    Remove oldest settings

--- cookbooks/emacs/files/.emacs.d/inits/linux-keybinds.el
+++
@@ -1,4 +0,0 @@
-(define-key global-map [(super z)] 'undo)
-(define-key global-map [(super c)] 'copy-region-as-kill)
-(define-key global-map [(super v)] 'yank)
-(define-key global-map [(super x)] 'kill-region)
--- cookbooks/emacs/files/.emacs.d/inits/nw-replace-colorthemes.el
+++
@@ -1,9 +0,0 @@
-(add-to-list 'custom-theme-load-path
-             (file-name-as-directory "~/.emacs.d/elisp/replace-colorthemes"))
-
-(load-theme 'manoj-dark t t)
-(enable-theme 'manoj-dark)
-
-;; linum の背景色
-(custom-set-faces
- '(linum ((t (:inherit (shadow default) :background "Gray9")))))
