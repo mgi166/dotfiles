556286bb55bd8af95a457095662a1cf6a849ea4e
mgi166 <hiromu.mogi@gmail.com>
2022-12-17 19:11:06 +0900

    Set markdown-preview-mode

--- cookbooks/emacs/files/.emacs.d/inits/80_markdown-mode.el
+++ cookbooks/emacs/files/.emacs.d/inits/80_markdown-mode.el
@@ -1,7 +1,10 @@
 (use-package markdown-mode
   :ensure t
-;  :init (add-hook 'markdown-mode-hook 'markdown-preview-mode)
   :bind (:map markdown-mode-map
               ("M-n" . markdown-forward-paragraph)
-              ("M-p" . markdown-backward-paragraph))
+              ("M-p" . markdown-backward-paragraph)))
+
+(use-package markdown-preview-mode
+  :ensure t
+  ;:init (add-hook 'markdown-mode-hook 'markdown-preview-mode)
   :config (setq markdown-preview-stylesheets (list "github.css")))
