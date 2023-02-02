4a6a9afc081b0e9411beea14d48e31323022c3e8
mgi166 <hiromu.mogi@gmail.com>
2023-02-02 19:29:57 +0900

    Upgrade typescript-mode with tide-mode

--- cookbooks/emacs/files/.emacs.d/inits/80_typescript-mode.el
+++ cookbooks/emacs/files/.emacs.d/inits/80_typescript-mode.el
@@ -2,3 +2,10 @@
   :ensure t
   :config (subword-mode 1)
           (custom-set-variables '(typescript-indent-level 2)))
+
+(use-package tide
+  :ensure t
+  :after (typescript-mode company flycheck)
+  :hook ((typescript-mode . tide-setup)
+         (typescript-mode . tide-hl-identifier-mode)
+         (before-save . tide-format-before-save)))
