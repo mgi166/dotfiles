c46e1a706b2003476a02b0dd627f446016eea246
mgi166 <hiromu.mogi@gmail.com>
2024-12-02 18:57:31 +0900

    Remove `M-s`, `M-r`. Conflict isearch prefix key M-s

--- cookbooks/emacs/files/.emacs.d/inits/25_helm-gtags.el
+++ cookbooks/emacs/files/.emacs.d/inits/25_helm-gtags.el
@@ -1,6 +1,6 @@
 (use-package helm-gtags
   :ensure t
   :bind ("M-t" . helm-gtags-find-tag)
-        ("M-r" . helm-gtags-find-rtag)
-        ("M-s" . helm-gtags-find-symbol)
+        ;; ("M-r" . helm-gtags-find-rtag)
+        ;; ("M-s" . helm-gtags-find-symbol)
         ("C-t" . helm-gtags-pop-stack))
