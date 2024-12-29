dffef646e970deb9c121d1ed0a38a75506226ba1
mgi166 <hiromu.mogi@gmail.com>
2024-12-29 20:29:53 +0900

    Remove auto-complete.

--- cookbooks/emacs/files/.emacs.d/inits/21_auto-complete.el
+++ cookbooks/emacs/files/.emacs.d/inits/21_auto-complete.el
@@ -1,26 +1,26 @@
 ;; auto-complite.el(自動補完機能)
-(require 'auto-complete)
-(require 'auto-complete-config)
+;; (require 'auto-complete)
+;; (require 'auto-complete-config)
 
-(set-default 'ac-sources
-             '(ac-source-abbrev
-               ac-source-dictionary
-               ac-source-yasnippet
-               ac-source-words-in-buffer
-               ac-source-words-in-same-mode-buffers
-               ac-source-semantic))
+;; (set-default 'ac-sources
+;;              '(ac-source-abbrev
+;;                ac-source-dictionary
+;;                ac-source-yasnippet
+;;                ac-source-words-in-buffer
+;;                ac-source-words-in-same-mode-buffers
+;;                ac-source-semantic))
 
-(ac-config-default)
+;; (ac-config-default)
 
-(dolist (mode '(ruby-mode))
-  (add-to-list 'ac-modes mode))
+;; (dolist (mode '(ruby-mode))
+;;   (add-to-list 'ac-modes mode))
 
-;;補完機能
-(setq partial-complication-mode 1)
+;; ;;補完機能
+;; (setq partial-complication-mode 1)
 
-(global-auto-complete-mode t)
+;; (global-auto-complete-mode t)
 
-(add-hook 'auto-complete-mode-hook
-          (lambda ()
-            (define-key ac-completing-map (kbd "C-n") 'ac-next)
-            (define-key ac-completing-map (kbd "C-p") 'ac-previous)))
+;; (add-hook 'auto-complete-mode-hook
+;;           (lambda ()
+;;             (define-key ac-completing-map (kbd "C-n") 'ac-next)
+;;             (define-key ac-completing-map (kbd "C-p") 'ac-previous)))
