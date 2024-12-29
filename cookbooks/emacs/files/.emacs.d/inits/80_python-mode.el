0c28df2dfe7a5cb13420066109eb493d87671f20
mgi166 <hiromu.mogi@gmail.com>
2024-12-29 20:28:38 +0900

    Refactor. update hooks, packages ensure

--- cookbooks/emacs/files/.emacs.d/inits/80_python-mode.el
+++ cookbooks/emacs/files/.emacs.d/inits/80_python-mode.el
@@ -2,8 +2,6 @@
   :ensure t
   :init (add-hook 'python-mode-hook 'highlight-indent-guides-mode)
         (add-hook 'python-mode-hook 'subword-mode)
-        (add-hook 'python-mode-hook 'python-isort-on-save-mode)
-        (add-hook 'python-mode-hook 'python-black-on-save-mode-enable-dwim)
 
 ;  :hook (python-mode-hook . jedi:setup)
   :hook (python-mode . lsp-deferred)
@@ -17,11 +15,13 @@
             (add-to-list 'lsp-disabled-clients 'pyls)
             (add-to-list 'lsp-enabled-clients 'jedi)))
 
-(use-package 'python-isort
-  :ensure t)
-
-(use-package 'python-black
+(use-package python-isort
   :ensure t
+  :hook (python-mode . python-isort-on-save-mode))
+
+(use-package python-black
+  :ensure t
+  :hook (python-mode . python-black-on-save-mode-enable-dwim)
   :after python)
 
 (use-package company-jedi
