fd6c0c7ebefe5c2862840a858ed3267d182c876c
mgi166 <hiromu.mogi@gmail.com>
2024-12-29 15:54:56 +0900

    Set :ensure t, globa-hl-line-mode t

--- cookbooks/emacs/files/.emacs.d/inits/21_hi-line.el
+++ cookbooks/emacs/files/.emacs.d/inits/21_hi-line.el
@@ -1,9 +1,12 @@
 (use-package hl-line
+  :ensure t
   :init (defun global-hl-line-timer-function ()
           (global-hl-line-unhighlight-all)
           (let ((global-hl-line-mode t))
             (global-hl-line-highlight)))
         (setq global-hl-line-timer
-              (run-with-idle-timer 0.01 t 'global-hl-line-timer-function)))
+              (run-with-idle-timer 0.01 t 'global-hl-line-timer-function))
+  :config (global-hl-line-mode t))
 
 ;; (cancel-timer global-hl-line-timer)
+
