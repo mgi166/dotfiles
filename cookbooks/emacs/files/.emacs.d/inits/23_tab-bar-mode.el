de35b3a9029f0e70d088f22630d580d08c8d897d
mgi166 <hiromu.mogi@gmail.com>
2024-12-30 10:51:08 +0900

    More useful tab-close

--- cookbooks/emacs/files/.emacs.d/inits/23_tab-bar-mode.el
+++ cookbooks/emacs/files/.emacs.d/inits/23_tab-bar-mode.el
@@ -3,13 +3,30 @@
 (define-key global-map (kbd "C-z") ctl-z-map)
 (tab-bar-history-mode 1)
 
-(defun my/tab-close-with-buffer ()
-  "tab close + kill buffer"
-  (interactive)
-  (kill-this-buffer)
-  (tab-close))
+(require 'cl-lib)
 
-(define-key ctl-z-map (kbd "k") 'my/tab-close-with-buffer)
+(defun my/remove-first-if (func list)
+  "Remove the first element from list that satisfies func."
+  (let (found)
+    (cl-loop for x in list
+             unless (and (not found) (funcall func x))
+             collect x
+             do (when (funcall func x) (setq found t)))))
+
+(defun my/exist-p-tab-bar-except-self (name)
+  "Whether target tab exists in current tabs except self."
+  (let* ((tab-bar-tabs-without-self (my/remove-first-if (lambda (tab) (equal (alist-get 'name tab) name)) (funcall tab-bar-tabs-function))))
+    (cl-some (lambda (tab) (equal (alist-get 'name tab) name))
+             tab-bar-tabs-without-self)))
+
+(defun my/tab-close ()
+  "tab close + kill buffer if current tab is duplicated otherwise tab close only"
+  (interactive)
+  (if (my/exist-p-tab-bar-except-self (alist-get 'name (tab-bar--current-tab-make)))
+      (tab-close)
+    (progn (kill-this-buffer) (tab-close))))
+
+(define-key ctl-z-map (kbd "k") 'my/tab-close)
 (define-key ctl-z-map (kbd "u") 'tab-undo)
 (define-key ctl-z-map (kbd "d") 'dired-other-tab)
 (define-key ctl-z-map (kbd "C-s") 'tab-move)
