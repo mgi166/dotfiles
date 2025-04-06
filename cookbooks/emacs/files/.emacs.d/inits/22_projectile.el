46d7c921e79d8ddbd35940cd4d75a24c75d995eb
mgi166 <hiromu.mogi@gmail.com>
2025-04-06 15:29:03 +0900

    Create map alias

--- cookbooks/emacs/files/.emacs.d/inits/22_projectile.el
+++ cookbooks/emacs/files/.emacs.d/inits/22_projectile.el
@@ -2,4 +2,5 @@
   :ensure t
   :init (projectile-mode +1)
   :bind (:map projectile-mode-map
-              ("C-c C-p" . projectile-command-map)))
+              ("C-c C-p" . projectile-command-map)
+              ("C-c p" . projectile-command-map)))
