af5b959aa739f420479d7c853b10434b7e66d030
mgi166 <hiromu.mogi@gmail.com>
2025-04-20 10:01:43 +0900

    Bind explicitly

--- cookbooks/emacs/files/.emacs.d/inits/22_projectile.el
+++ cookbooks/emacs/files/.emacs.d/inits/22_projectile.el
@@ -1,6 +1,7 @@
 (use-package projectile
   :ensure t
   :init (projectile-mode +1)
-  :bind (:map projectile-mode-map
+  :bind ("C-c p t" . projectile-toggle-between-implementation-and-test)
+        (:map projectile-mode-map
               ("C-c C-p" . projectile-command-map)
               ("C-c p" . projectile-command-map)))
