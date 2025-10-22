f91b90ba96c7d187ccce3d30a16f4c73b94bc2a9
mgi166 <hiromu.mogi@gmail.com>
2025-10-22 14:38:15 +0900

    Fix load order

--- cookbooks/zsh/files/.zsh/all.zsh
+++ cookbooks/zsh/files/.zsh/all.zsh
@@ -1,4 +1,5 @@
 [ -f $HOME/.zsh/env.zsh ]        && source $HOME/.zsh/env.zsh
+[ -f $HOME/.zsh/path.zsh ]       && source $HOME/.zsh/path.zsh
 [ -f $HOME/.zsh/completion.zsh ] && source $HOME/.zsh/completion.zsh
 [ -f $HOME/.zsh/alias.zsh ]      && source $HOME/.zsh/alias.zsh
 [ -f $HOME/.zsh/color.zsh ]      && source $HOME/.zsh/color.zsh
@@ -6,7 +7,6 @@
 [ -f $HOME/.zsh/history.zsh ]    && source $HOME/.zsh/history.zsh
 [ -f $HOME/.zsh/option.zsh ]     && source $HOME/.zsh/option.zsh
 [ -f $HOME/.zsh/prompt.zsh ]     && source $HOME/.zsh/prompt.zsh
-[ -f $HOME/.zsh/path.zsh ]       && source $HOME/.zsh/path.zsh
 
 # environment dependence file
 [ -f $HOME/.zsh/.zshrc_local ]   && . $HOME/.zsh/.zshrc_local
