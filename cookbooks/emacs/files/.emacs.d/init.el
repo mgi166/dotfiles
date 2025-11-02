b59b02e83e8aa29e566ef6ba798fc78d2c0e1277
mgi166 <hiromu.mogi@gmail.com>
2025-11-02 14:49:32 +0900

    Refactor add-to-load-path method

--- cookbooks/emacs/files/.emacs.d/init.el
+++ cookbooks/emacs/files/.emacs.d/init.el
@@ -3,18 +3,14 @@
 
 ;; 引数の directory とその sub_directory を load-path に追加
 (defun add-to-load-path (&rest paths)
-   (let (path)
-     (dolist (path paths paths)
-       (let ((default-directory (expand-file-name (concat user-emacs-directory path))))
-         (add-to-list 'load-path default-directory)
-         (if (fboundp 'normal-top-level-add-subdirs-to-load-path)
-             (normal-top-level-add-subdirs-to-load-path))))))
+  (dolist (p paths)
+    (let ((default-directory
+           (expand-file-name (concat user-emacs-directory p))))
+      (add-to-list 'load-path default-directory)
+      (when (fboundp 'normal-top-level-add-subdirs-to-load-path)
+        (normal-top-level-add-subdirs-to-load-path)))))
 
-(add-to-load-path "elisp")
-(add-to-load-path "site-lisp")
-
-;; https://github.com/syl20bnr/spacemacs/issues/12535
-(setq gnutls-algorithm-priority "NORMAL:-VERS-TLS1.3")
+(add-to-load-path "elisp" "site-lisp")
 
 ;; package.el
 ;; M-x package-refresh-contents で repository を最新にする
