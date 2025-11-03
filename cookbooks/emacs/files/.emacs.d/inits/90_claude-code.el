f85b4179eac9245bd840f4efa2a020b20653201e
mgi166 <hiromu.mogi@gmail.com>
2025-11-03 10:15:29 +0900

    Install claude-code.el

---
+++ cookbooks/emacs/files/.emacs.d/inits/90_claude-code.el
@@ -0,0 +1,19 @@
+;; install required inheritenv dependency
+(use-package inheritenv
+  :ensure t)
+
+(use-package claude-code
+  :ensure t
+  :straight (:type git :host github :repo "stevemolitor/claude-code.el")
+  :config
+  (claude-code-mode)
+
+  ;; Optionally define a repeat map so that "M" will cycle thru Claude auto-accept/plan/confirm modes after invoking claude-code-cycle-mode / C-c M.
+  :bind
+  (:repeat-map my-claude-code-map ("M" . claude-code-cycle-mode)))
+
+(use-package claude-code-ide
+  :straight (:type git :host github :repo "manzaltu/claude-code-ide.el")
+  :bind ("C-c a" . claude-code-ide-menu)
+  :config
+  (claude-code-ide-emacs-tools-setup)) ; Optionally enable Emacs MCP tools
