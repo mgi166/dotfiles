8135b2ef6332fd5588eb1794c0e3f88efd1a5b2a
mgi166 <hiromu.mogi@gmail.com>
2025-11-08 13:42:57 +0900

    Update vterm configuration

--- cookbooks/emacs/files/.emacs.d/inits/40_vterm.el
+++ cookbooks/emacs/files/.emacs.d/inits/40_vterm.el
@@ -1,2 +1,11 @@
 (use-package vterm
-  :ensure t)
+  :straight t
+  :bind
+  ;; vterm 上で 1 文字消せないので明示的に関数を bind
+  (:map vterm-mode-map
+              ("C-h" . vterm-send-backspace))
+  ;; vterm 上でも tab-bar-mode の keybind prefix を使用したいので、C-z は emacs に送る
+  :config
+  (customize-set-variable
+   'vterm-keymap-exceptions
+   (append vterm-keymap-exceptions '("C-z"))))
