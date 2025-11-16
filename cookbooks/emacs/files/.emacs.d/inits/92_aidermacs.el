9e3b8c97de2beb81091f5217fb00fce32319ad2d
mgi166 <hiromu.mogi@gmail.com>
2025-11-16 20:22:35 +0900

    Remove aidrmacs config

--- cookbooks/emacs/files/.emacs.d/inits/92_aidermacs.el
+++
@@ -1,7 +0,0 @@
-(use-package aidermacs
-  :ensure t
-  :bind (("C-c a" . aidermacs-transient-menu))
-  :custom
-  ; See the Configuration section below
-  (aidermacs-use-architect-mode t)
-  (aidermacs-default-model "sonnet"))
