ba8295f9baff5a9ba69882428338acab6ea8ec93
mgi166 <hiromu.mogi@gmail.com>
2025-11-17 17:18:42 +0900

    Update rust-mode settings

--- cookbooks/emacs/files/.emacs.d/inits/80_rust-mode.el
+++ cookbooks/emacs/files/.emacs.d/inits/80_rust-mode.el
@@ -1,41 +1,19 @@
 ;; https://robert.kra.hn/posts/2021-02-07_rust-with-emacs/
 (add-to-list 'exec-path (expand-file-name "~/.cargo/bin"))
 
-(defun kill-buffer-compiration ()
+(defun rust-kill-buffer-compiration ()
   "Kill compiration buffer with no confirm"
   (interactive)
   (let ((kill-buffer-query-functions nil)
         (kill-buffer "*compilation*")))
   (delete-other-windows))
 
-; ref: https://stackoverflow.com/questions/9725015/how-do-i-make-the-compilation-window-in-emacs-to-always-be-a-certain-size
-
 (use-package rust-mode
   :ensure t
-  :custom rust-format-on-save t
+  :custom (rust-format-on-save t)
   :config (add-to-list 'lsp-enabled-clients 'rust-analyzer)
-          (add-to-list 'lsp-enabled-clients 'rls)
-  :bind ("C-c c" . rust-run)
-        ("C-c C-v" . rust-run)
-        ("C-c t" . rust-test)
-        ("C-c q" . kill-buffer-compiration))
-
-(use-package cargo
-  :ensure t
-  :hook (rust-mode . cargo-minor-mode))
-
-;(dap-register-debug-template
-; "LLDB::Run with lldb-vscode"
-;  (list :type "lldb"
-;        :cmd "~/.cargo/bin/rust-lldb"
-;        :request "launch"
-;        :program "target/debug/rpn"
-;        :miDebuggerPath "~/.vscode/extensions/ms-vscode.cpptools-1.1.3/debugAdapters/lldb-mi/bin/"
-;        :name "LLDB::Run"))
-
-;(dap-register-debug-template
-; "LLDB::GDB rust-gdb"
-;  (list :type "gdb"
-;        :request "launch"
-;        :name "LLDB::Run"
-;        :gdbpath "rust-gdb"))
+  :bind (("C-c C-r r" . rust-run)
+        ("C-c C-r t" . rust-test)
+        ("C-c C-r c" . rust-compile)
+        ("C-c C-r k" . rust-check)
+        ("C-c C-r q" . rust-kill-buffer-compiration)))
